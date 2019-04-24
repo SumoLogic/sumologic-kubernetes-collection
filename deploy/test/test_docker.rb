54dfdea2154835ef24e249d4e4094f048ab14832
Bin Yi <byi@sumologic.com>
2019-04-24 13:04:06 -0700

    update docker unit test

--- deploy/test/test_docker.rb
+++ deploy/test/test_docker.rb
@@ -1,7 +1,6 @@
 require 'test/unit'
 
 class TestDocker < Test::Unit::TestCase
-  DOCKER_TAG = 'sumologic/kubernetes-fluentd'.freeze
   CONTAINER_NAME = 'test-container'.freeze
   DUMMY_OUTPUT = 'dummy: {"hello":"world"}'.freeze
 
@@ -13,22 +12,25 @@ class TestDocker < Test::Unit::TestCase
     system("docker rm -f #{CONTAINER_NAME}", out: File::NULL, err: File::NULL)
   end
 
+  def docker_tag
+    ENV['DOCKER_TAG'].nil? ? 'sumologic/kubernetes-fluentd' : ENV['DOCKER_TAG']
+  end
+
   def test_docker_image_exist
     result = `docker images`
-    assert result.include?(DOCKER_TAG)
+    assert result.include?(docker_tag)
   end
 
   def test_docker_image_runnable
-    id = `docker run -d --rm --name #{CONTAINER_NAME} #{DOCKER_TAG}:latest`
+    id = `docker run -d --rm --name #{CONTAINER_NAME} #{docker_tag}:latest`
     assert !id.nil? && !id.empty?
-    [1..20].each do |i|
-      sleep 1
-      result = `docker ps --filter "name=#{CONTAINER_NAME}"`
-      assert(
-        result.include?(CONTAINER_NAME),
-        "container stopped after #{i} seconds"
-      )
-    end
+    sleep_time = 15
+    sleep sleep_time
+    result = `docker ps --filter "name=#{CONTAINER_NAME}"`
+    assert(
+      result.include?(CONTAINER_NAME),
+      "container stopped after #{sleep_time} seconds"
+    )
     logs = `docker logs #{CONTAINER_NAME}`
     puts logs
     assert logs.include?(DUMMY_OUTPUT)
