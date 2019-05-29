09d85314e66c6621a0f450335d21cf40c1c41e49
Bin Yi <byi@sumologic.com>
2019-05-29 16:03:35 -0700

    read owners

--- fluent-plugin-enhance-k8s-metadata/lib/sumologic/kubernetes/reader.rb
+++ fluent-plugin-enhance-k8s-metadata/lib/sumologic/kubernetes/reader.rb
@@ -4,11 +4,41 @@ module SumoLogic
     module Reader
       require_relative 'connector.rb'
 
+      MAX_BFS_DEPTH = 8
+
+      # from https://kubernetes.io/docs/reference/kubectl/overview/#resource-types
+      RESOURCE_MAPPING = {
+        'ConfigMap' => 'configmaps',
+        'ControllerRevision' => 'controllerrevisions',
+        'CronJob' => 'cronjobs',
+        'DaemonSet' => 'daemonsets',
+        'Deployment' => 'deployments',
+        'Endpoints' => 'endpoints',
+        'Event' => 'events',
+        'HorizontalPodAutoscaler' => 'horizontalpodautoscalers',
+        'Ingress' => 'ingresses',
+        'Job' => 'jobs',
+        'Lease' => 'leases',
+        'LimitRange' => 'limitranges',
+        'LocalSubjectAccessReview' => 'localsubjectaccessreviews',
+        'NetworkPolicy' => 'networkpolicies',
+        'PersistentVolumeClaim' => 'persistentvolumeclaims',
+        'Pod' => 'pods',
+        'PodDisruptionBudget' => 'poddisruptionbudgets',
+        'PodTemplate' => 'podtemplates',
+        'ReplicaSet' => 'replicasets',
+        'ReplicationController' => 'replicationcontrollers',
+        'ResourceQuota' => 'resourcequotas',
+        'Role' => 'roles',
+        'RoleBinding' => 'rolebindings',
+        'Secret' => 'secrets',
+        'Service' => 'services',
+        'ServiceAccount' => 'serviceaccounts',
+        'StatefulSet' => 'statefulsets'
+      }.freeze
+
       def fetch_pod(namespace_name, pod_name)
-        log.info "fetching pod metadata: #{namespace_name}::#{pod_name}"
-        pod = @client.get_pod(pod_name, namespace_name)
-        log.debug "raw metadata for #{namespace_name}::#{pod_name}: #{pod}"
-        pod
+        fetch_resource('pods', pod_name, namespace_name)
       end
 
       def extract_pod_labels(pod)
@@ -29,6 +59,48 @@ module SumoLogic
         log.error e
         # TODO: we now cache empty if not found since some namespace/pod not matching
         {}
+      end
+
+      def fetch_owners(namespace_name, resource)
+        # BFS for owners
+        result = []
+        depth = 0
+        queue = [resource]
+        visited = {
+          resource['metadata']['uid'] => resource
+        }
+        while !queue.empty? && depth <= MAX_BFS_DEPTH
+          size = queue.size
+          [1..size].each do
+            current = queue.shift
+            result.add(current)
+            owner_references = current['ownerReferences']
+            begin
+              owner_references.each do |owner_reference|
+                next if visited.key?(owner_reference['uid'])
+
+                owner = fetch_resource(
+                  RESOURCE_MAPPING[owner_reference['kind']],
+                  owner_reference['name'],
+                  namespace_name
+                )
+                queue.push(owner)
+                visited[owner_reference['uid']] = owner
+              end
+            rescue StandardError => e
+              log.error e
+            end
+          end
+          depth += 1
+        end
+        result
+      end
+
+      def fetch_resource(type, name, namespace)
+        log.info "fetching resource: #{type}, name: #{name}, ns:#{namespace}"
+        resource = @client.get_entity(type, name, namespace)
+        log.debug resource.to_s
+        resource
       end
     end
   end
