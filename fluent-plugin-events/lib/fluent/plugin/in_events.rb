62d0da0f977dcf69365b585bae92b998c256d758
Maisie Wang <maisie@sumologic.com>
2019-05-31 16:28:11 -0700

    Use bin's connector

--- fluent-plugin-events/lib/fluent/plugin/in_events.rb
+++ fluent-plugin-events/lib/fluent/plugin/in_events.rb
@@ -6,16 +6,28 @@ module Fluent
     class EventsInput < Fluent::Plugin::Input
       Fluent::Plugin.register_input("events", self)
 
+      require_relative '../../sumologic/kubernetes/connector.rb'
+
       helpers :thread
 
+      include SumoLogic::Kubernetes::Connector
+      
+      # parameters for connecting to k8s api server
       config_param :kubernetes_url, :string, default: nil
-      config_param :api_version, :string, default: 'v1'
+      config_param :apiVersion, :string, default: 'v1'
       config_param :client_cert, :string, default: nil
       config_param :client_key, :string, default: nil
       config_param :ca_file, :string, default: nil
-      config_param :insecure_ssl, :bool, default: false
-      config_param :bearer_token_file, :string, default: nil
       config_param :secret_dir, :string, default: '/var/run/secrets/kubernetes.io/serviceaccount'
+      config_param :bearer_token_file, :string, default: nil
+      config_param :verify_ssl, :bool, default: true
+      # if `ca_file` is for an intermediate CA, or otherwise we do not have the
+      # root CA and want to trust the intermediate CA certs we do have, set this
+      # to `true` - this corresponds to the openssl s_client -partial_chain flag
+      # and X509_V_FLAG_PARTIAL_CHAIN
+      config_param :ssl_partial_chain, :bool, default: false
+      
+      # parameters for events collection
       config_param :tag, :string, default: 'kubernetes.*'
       config_param :namespace, :string, default: nil
       config_param :label_selector, :string, default: nil
@@ -23,7 +35,8 @@ module Fluent
       
       def configure(conf)
         super
-        initialize_client
+        normalize_param
+        connect_kubernetes
       end
 
       def start
@@ -38,64 +51,12 @@ module Fluent
         super
       end
 
-      def initialize_client
-        log.trace { "Initializing client" }
-        # mostly borrowed from Fluentd Kubernetes Metadata Filter Plugin
-        if @kubernetes_url.nil?
-          # Use Kubernetes default service account if we're in a pod.
-          env_host = ENV['KUBERNETES_SERVICE_HOST']
-          env_port = ENV['KUBERNETES_SERVICE_PORT']
-          if env_host && env_port
-            @kubernetes_url = "https://#{env_host}:#{env_port}/#{@api_version == 'v1' ? 'api' : 'apis'}"
-          end
-        end
-  
-        raise Fluent::ConfigError, 'kubernetes url is not set' unless @kubernetes_url
-  
-        # Use SSL certificate and bearer token from Kubernetes service account.
-        if Dir.exist?(@secret_dir)
-          secret_ca_file = File.join(@secret_dir, 'ca.crt')
-          secret_token_file = File.join(@secret_dir, 'token')
-  
-          if @ca_file.nil? && File.exist?(secret_ca_file)
-            @ca_file = secret_ca_file
-          end
-  
-          if @bearer_token_file.nil? && File.exist?(secret_token_file)
-            @bearer_token_file = secret_token_file
-          end
-        end
-  
-        ssl_options = {
-          client_cert: @client_cert && OpenSSL::X509::Certificate.new(File.read(@client_cert)),
-          client_key: @client_key && OpenSSL::PKey::RSA.new(File.read(@client_key)),
-          ca_file: @ca_file,
-          verify_ssl: @insecure_ssl ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
-        }
-  
-        auth_options = {}
-        auth_options[:bearer_token] = File.read(@bearer_token_file) if @bearer_token_file
-  
-        @client = Kubeclient::Client.new(
-          @kubernetes_url, @api_version,
-          ssl_options: ssl_options,
-          auth_options: auth_options
-        )
-  
-        begin
-            @client.api_valid?
-        rescue KubeException => kube_error
-          raise Fluent::ConfigError, "Invalid Kubernetes API #{@api_version} endpoint #{@kubernetes_url}: #{kube_error.message}"
-          end
-      end
-
       def initialize_resource_version
         # get or create the config map
         begin
           @client.public_send("get_config_map", "fluentd-config-resource-version", "sumologic").tap do |resource|
-            log.trace {"Get config maps: #{resource}"}
+            log.info {"Get config maps: #{resource}"}
             version = resource.data['resource-version']
-            log.trace { "Get version from config map: #{version}"}
             @resource_version = version if version
           end
         rescue Kubeclient::ResourceNotFoundError
@@ -112,7 +73,7 @@ module Fluent
         }
         resource.data = { "resource-version": "#{@resource_version}" }
         @client.public_send("create_config_map", resource).tap do |maps|
-          log.trace {"Created config maps: #{maps}"}
+          log.info {"Created config maps: #{maps}"}
         end
       end
   
@@ -127,7 +88,7 @@ module Fluent
         @watcher = @client.public_send("watch_events", params).tap do |watcher|
           thread_create(:"watch_events") do
             watcher.each do |entity|
-              log.trace { "Received new object from watching events" }
+              log.debug { "Received new object from watching events" }
               entity = JSON.parse(entity)
               router.emit tag, Fluent::Engine.now, entity
               @resource_version = entity['object']['metadata']['resourceVersion']
@@ -138,8 +99,6 @@ module Fluent
                 start_watcher_thread
                 break
               end
-  
-              log.trace { "resource version: #{entity['object']['metadata']['resourceVersion']}"}
             end
           end
         end
@@ -158,11 +117,28 @@ module Fluent
 
             # update the config map
             @client.public_send("update_config_map", resource).tap do |maps|
-              log.trace {"Updated config maps: #{maps}"}
+              log.debug {"Updated config maps: #{maps}"}
             end
           end
         end
-      end 
+      end
+
+      def normalize_param
+        # Use Kubernetes default service account if running in a pod.
+        if @kubernetes_url.nil?
+          log.debug 'Kubernetes URL is not set - inspecting environment'
+          env_host = ENV['KUBERNETES_SERVICE_HOST']
+          env_port = ENV['KUBERNETES_SERVICE_PORT']
+          @kubernetes_url = "https://#{env_host}:#{env_port}/api" unless env_host.nil? || env_port.nil?
+        end
+        log.info "Kubernetes URL: '#{@kubernetes_url}'"
+
+        @ca_file = File.join(@secret_dir, K8_POD_CA_CERT) if @ca_file.nil?
+        log.info "ca_file: '#{@ca_file}', exist: #{File.exist?(@ca_file)}"
+
+        @bearer_token_file = File.join(@secret_dir, K8_POD_TOKEN) if @bearer_token_file.nil?
+        log.info "bearer_token_file: '#{@bearer_token_file}', exist: #{File.exist?(@bearer_token_file)}"
+      end
     end
   end
 end
