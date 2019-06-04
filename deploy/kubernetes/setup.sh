ce15dc1cd009645e0064d36cf4856d649b7b344e
Sam Song <ssong@sumologic.com>
2019-06-04 13:58:04 -0700

    add -metrics to metrics source names

--- deploy/kubernetes/setup.sh
+++ deploy/kubernetes/setup.sh
@@ -109,25 +109,25 @@ create_host_collector $COLLECTOR_NAME
 
 echo "Creating sources in '$COLLECTOR_NAME'..."
 SOURCE_URL=
-create_http_source '(default)' $COLLECTOR_ID
+create_http_source '(default-metrics)' $COLLECTOR_ID
 ENDPOINT_METRICS="$SOURCE_URL"
 SOURCE_URL=
-create_http_source apiserver $COLLECTOR_ID
+create_http_source apiserver-metrics $COLLECTOR_ID
 ENDPOINT_METRICS_APISERVER="$SOURCE_URL"
 SOURCE_URL=
-create_http_source kube-controller-manager $COLLECTOR_ID
+create_http_source kube-controller-manager-metrics $COLLECTOR_ID
 ENDPOINT_METRICS_KUBE_CONTROLLER_MANAGER="$SOURCE_URL"
 SOURCE_URL=
-create_http_source kube-scheduler $COLLECTOR_ID
+create_http_source kube-scheduler-metrics $COLLECTOR_ID
 ENDPOINT_METRICS_KUBE_SCHEDULER="$SOURCE_URL"
 SOURCE_URL=
-create_http_source kube-state $COLLECTOR_ID
+create_http_source kube-state-metrics $COLLECTOR_ID
 ENDPOINT_METRICS_KUBE_STATE="$SOURCE_URL"
 SOURCE_URL=
-create_http_source kubelet $COLLECTOR_ID
+create_http_source kubelet-metrics $COLLECTOR_ID
 ENDPOINT_METRICS_KUBELET="$SOURCE_URL"
 SOURCE_URL=
-create_http_source node-exporter $COLLECTOR_ID
+create_http_source node-exporter-metrics $COLLECTOR_ID
 ENDPOINT_METRICS_NODE_EXPORTER="$SOURCE_URL"
 
 kubectl -n sumologic create secret generic sumologic \
