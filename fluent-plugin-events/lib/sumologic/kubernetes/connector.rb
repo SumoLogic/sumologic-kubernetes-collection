64c33fd2a81b857402625c0a75a6a8a5f34b4a64
Yuting Liu <yuting@sumologic.com>
2019-07-08 22:52:09 -0700

    SUMO-111064 Fix connector

--- fluent-plugin-events/lib/sumologic/kubernetes/connector.rb
+++ fluent-plugin-events/lib/sumologic/kubernetes/connector.rb
@@ -17,25 +17,43 @@ module SumoLogic
         rescue Exception => e
           log.error e
         end
-        if !@client_cert.nil? && File.exist?(@client_cert)
-          ssl_options[:client_cert] = OpenSSL::X509::Certificate.new(File.read(@client_cert))
+  
+        def ssl_store
+          require 'openssl'
+          ssl_store = OpenSSL::X509::Store.new
+          ssl_store.set_default_paths
+          # if version of ruby does not define OpenSSL::X509::V_FLAG_PARTIAL_CHAIN
+          flagval = 0x80000
+          flagval = OpenSSL::X509::V_FLAG_PARTIAL_CHAIN if defined? OpenSSL::X509::V_FLAG_PARTIAL_CHAIN
+          ssl_store.flags = OpenSSL::X509::V_FLAG_CRL_CHECK_ALL | flagval
+          ssl_store
         end
-        if !@client_key.nil? && File.exist?(@client_key)
-          ssl_options[:client_key] = OpenSSL::PKey::RSA.new(File.read(@client_key))
+  
+        def ssl_options
+          ssl_options = {}
+          ssl_options[:verify_ssl] = @verify_ssl ? OpenSSL::SSL::VERIFY_PEER : OpenSSL::SSL::VERIFY_NONE
+          if !@ca_file.nil? && File.exist?(@ca_file)
+            ssl_options[:ca_file] = @ca_file
+          end
+          if !@client_cert.nil? && File.exist?(@client_cert)
+            ssl_options[:client_cert] = OpenSSL::X509::Certificate.new(File.read(@client_cert))
+          end
+          if !@client_key.nil? && File.exist?(@client_key)
+            ssl_options[:client_key] = OpenSSL::PKey::RSA.new(File.read(@client_key))
+          end
+          ssl_options[:cert_store] = ssl_store if @ssl_partial_chain
+          log.debug "ssl_options: #{ssl_options}"
+          ssl_options
         end
-        ssl_options[:cert_store] = ssl_store if @ssl_partial_chain
-        log.debug "ssl_options: #{ssl_options}"
-        ssl_options
-      end
-
-      def auth_options
-        auth_options = {}
-        if !@bearer_token_file.nil? && File.exist?(@bearer_token_file)
-          auth_options[:bearer_token] = File.read(@bearer_token_file)
+  
+        def auth_options
+          auth_options = {}
+          if !@bearer_token_file.nil? && File.exist?(@bearer_token_file)
+            auth_options[:bearer_token] = File.read(@bearer_token_file)
+          end
+          log.debug "auth_options: #{ssl_options}"
+          auth_options
         end
-        log.debug "auth_options: #{ssl_options}"
-        auth_options
       end
     end
   end
-end
