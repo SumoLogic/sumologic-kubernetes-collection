5e46c30dd5469a677fd176956fea3cb9a4f39dcc
Travis CI <travis@travis-ci.org>
2019-08-19 15:49:33 -0700

    Fix eval

--- ci/build.sh
+++ ci/build.sh
@@ -73,7 +73,7 @@ if [ "$TRAVIS_BRANCH" != "master" ] && [ "$TRAVIS_EVENT_TYPE" == "push" ] && [ -
 
   with_files=`ls deploy/helm/sumologic/templates/*.yaml | sed 's#deploy/helm/sumologic/templates#-x templates#g' | sed 's/yaml/yaml \\\/g'`
   echo "$with_files"
-  eval "sudo helm template deploy/helm/sumologic --namespace "\$NAMESPACE" --set dryRun=true \ $with_files >> deploy/kubernetes/fluentd-sumologic.yaml.tmpl"
+  eval 'sudo helm template deploy/helm/sumologic $with_files --namespace "\$NAMESPACE" --set dryRun=true >> deploy/kubernetes/fluentd-sumologic.yaml.tmpl'
 
   if [[ $(git diff deploy/kubernetes/fluentd-sumologic.yaml.tmpl) ]]; then
       echo "Detected changes in 'fluentd-sumologic.yaml.tmpl', committing the updated version to $TRAVIS_BRANCH..."
