c3bc7a839af3a7198a80a08ccd01fd0456b1951a
Sam Song <ssong@sumologic.com>
2019-10-09 02:28:44 -0700

    cleanup some of filter plugin code

--- fluent-plugin-kubernetes-sumologic/lib/fluent/plugin/filter_kubernetes_sumologic.rb
+++ fluent-plugin-kubernetes-sumologic/lib/fluent/plugin/filter_kubernetes_sumologic.rb
@@ -57,51 +57,44 @@ module Fluent::Plugin
     end
 
     def filter(tag, time, record)
+      log_fields = {}
+
       # Set the sumo metadata fields
       sumo_metadata = record["_sumo_metadata"] || {}
       record["_sumo_metadata"] = sumo_metadata
-      log_fields = {}
       sumo_metadata[:log_format] = @log_format
       sumo_metadata[:host] = @source_host if @source_host
       sumo_metadata[:source] = @source_name if @source_name
-
       unless @source_category.nil?
         sumo_metadata[:category] = @source_category.dup
         unless @source_category_prefix.nil?
           sumo_metadata[:category].prepend(@source_category_prefix)
         end
       end
+      sumo_metadata[:category].gsub!("-", @source_category_replace_dash)
 
+      # Check systemd exclude filters
       if record.key?("_SYSTEMD_UNIT") and not record.fetch("_SYSTEMD_UNIT").nil?
         unless @exclude_unit_regex.empty?
-          if Regexp.compile(@exclude_unit_regex).match(record["_SYSTEMD_UNIT"])
-            return nil
-          end
+          return nil if Regexp.compile(@exclude_unit_regex).match(record["_SYSTEMD_UNIT"])
         end
-
         unless @exclude_facility_regex.empty?
-          if Regexp.compile(@exclude_facility_regex).match(record["SYSLOG_FACILITY"])
-            return nil
-          end
+          return nil if Regexp.compile(@exclude_facility_regex).match(record["SYSLOG_FACILITY"])
         end
-
         unless @exclude_priority_regex.empty?
-          if Regexp.compile(@exclude_priority_regex).match(record["PRIORITY"])
-            return nil
-          end
+          return nil if Regexp.compile(@exclude_priority_regex).match(record["PRIORITY"])
         end
-
         unless @exclude_host_regex.empty?
-          if Regexp.compile(@exclude_host_regex).match(record["_HOSTNAME"])
-            return nil
-          end
+          return nil if Regexp.compile(@exclude_host_regex).match(record["_HOSTNAME"])
         end
       end
 
-      # Allow fields to be overridden by annotations
       if record.key?("kubernetes") and not record.fetch("kubernetes").nil?
         # Clone kubernetes hash so we don't override the cache
+        # Note (sam 10/9/19): this is a shallow copy; nested hashes can still be overriden
         kubernetes = record["kubernetes"].clone
+
+        # Populate k8s_metadata to use later in sumo_metadata
         k8s_metadata = {
             :namespace => kubernetes["namespace_name"],
             :pod => kubernetes["pod_name"],
@@ -109,8 +102,6 @@ module Fluent::Plugin
             :container => kubernetes["container_name"],
             :source_host => kubernetes["host"],
         }
-
-
         if kubernetes.has_key? "labels"
           kubernetes["labels"].each { |k, v| k8s_metadata["label:#{k}".to_sym] = v }
         end
@@ -119,34 +110,22 @@ module Fluent::Plugin
         end
         k8s_metadata.default = "undefined"
 
+        # Fetch annotations for config
         annotations = kubernetes.fetch("annotations", {})
-        if annotations["sumologic.com/include"] == "true"
-          include = true
-        else
-          include = false
-        end
 
-        unless @exclude_namespace_regex.empty?
-          if Regexp.compile(@exclude_namespace_regex).match(k8s_metadata[:namespace]) and not include
-            return nil
+        unless annotations["sumologic.com/include"] == "true"
+          # Check kubernetes exclude filters
+          unless @exclude_namespace_regex.empty?
+            return nil if Regexp.compile(@exclude_namespace_regex).match(k8s_metadata[:namespace])
           end
-        end
-
-        unless @exclude_pod_regex.empty?
-          if Regexp.compile(@exclude_pod_regex).match(k8s_metadata[:pod]) and not include
-            return nil
+          unless @exclude_pod_regex.empty?
+            return nil if Regexp.compile(@exclude_pod_regex).match(k8s_metadata[:pod])
           end
-        end
-
-        unless @exclude_container_regex.empty?
-          if Regexp.compile(@exclude_container_regex).match(k8s_metadata[:container]) and not include
-            return nil
+          unless @exclude_container_regex.empty?
+            return nil if Regexp.compile(@exclude_container_regex).match(k8s_metadata[:container])
           end
-        end
-
-        unless @exclude_host_regex.empty?
-          if Regexp.compile(@exclude_host_regex).match(k8s_metadata[:source_host]) and not include
-            return nil
+          unless @exclude_host_regex.empty?
+            return nil if Regexp.compile(@exclude_host_regex).match(k8s_metadata[:source_host])
           end
         end
 
@@ -158,23 +137,18 @@ module Fluent::Plugin
 
         sumo_metadata[:log_format] = annotations["sumologic.com/format"] if annotations["sumologic.com/format"]
 
-        if annotations["sumologic.com/sourceHost"].nil?
-          sumo_metadata[:host] = sumo_metadata[:host] % k8s_metadata
-        else
-          sumo_metadata[:host] = annotations["sumologic.com/sourceHost"] % k8s_metadata
+        unless annotations["sumologic.com/sourceHost"].nil?
+          sumo_metadata[:host] = annotations["sumologic.com/sourceHost"]
         end
-
-        if annotations["sumologic.com/sourceName"].nil?
-          sumo_metadata[:source] = sumo_metadata[:source] % k8s_metadata
-        else
-          sumo_metadata[:source] = annotations["sumologic.com/sourceName"] % k8s_metadata
+        unless annotations["sumologic.com/sourceName"].nil?
+          sumo_metadata[:source] = annotations["sumologic.com/sourceName"]
         end
-
-        if annotations["sumologic.com/sourceCategory"].nil?
-          sumo_metadata[:category] = sumo_metadata[:category] % k8s_metadata
-        else
-          sumo_metadata[:category] = (annotations["sumologic.com/sourceCategory"] % k8s_metadata).prepend(@source_category_prefix)
+        unless annotations["sumologic.com/sourceCategory"].nil?
+          sumo_metadata[:category] = annotations["sumologic.com/sourceCategory"].dup.prepend(@source_category_prefix)
         end
+        sumo_metadata[:host] = sumo_metadata[:host] % k8s_metadata
+        sumo_metadata[:source] = sumo_metadata[:source] % k8s_metadata
+        sumo_metadata[:category] = sumo_metadata[:category] % k8s_metadata
         sumo_metadata[:category].gsub!("-", @source_category_replace_dash)
 
         # Strip kubernetes metadata from json if disabled
@@ -198,10 +172,12 @@ module Fluent::Plugin
           record.delete("time")
         end
         # Strip sumologic.com annotations
+        # Note (sam 10/9/19): we're stripping from the copy, so this has no affect on output
         kubernetes.delete("annotations") if annotations
 
         if @log_format == "fields" and record.key?("docker") and not record.fetch("docker").nil?
           record["docker"].each {|k, v| log_fields[k] = v}
+          record.delete("docker")
         end
 
         if @log_format == "fields" and record.key?("kubernetes") and not record.fetch("kubernetes").nil?
@@ -218,13 +194,12 @@ module Fluent::Plugin
             log_fields[key] = kubernetes[key] unless kubernetes[key].nil?
           end
           log_fields["node"] = kubernetes["host"] unless kubernetes["host"].nil?
+          record.delete("kubernetes")
         end
       end
 
       if @log_format == "fields" and not log_fields.nil?
         sumo_metadata[:fields] = log_fields.select{|k,v| !(v.nil? || v.empty?)}.map{|k,v| "#{k}=#{v}"}.join(',')
-        record.delete("docker")
-        record.delete("kubernetes")
       end
       record
     end
