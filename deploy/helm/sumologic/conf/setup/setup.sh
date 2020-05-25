4939f1a1be2b8ceab089673b6079e37ea32a9c29
Dominik Rosiek <drosiek@sumologic.com>
2020-05-25 10:38:28 +0200

    Add dynamic generation of terraform imports

--- deploy/helm/sumologic/conf/setup/setup.sh
+++ deploy/helm/sumologic/conf/setup/setup.sh
@@ -15,15 +15,9 @@ terraform init
 
 # Sumo Collector and HTTP sources
 terraform import sumologic_collector.collector "$COLLECTOR_NAME"
-terraform import sumologic_http_source.default_metrics_source "$COLLECTOR_NAME/(default-metrics)"
-terraform import sumologic_http_source.apiserver_metrics_source "$COLLECTOR_NAME/apiserver-metrics"
-terraform import sumologic_http_source.events_source "$COLLECTOR_NAME/events"
-terraform import sumologic_http_source.kube_controller_manager_metrics_source "$COLLECTOR_NAME/kube-controller-manager-metrics"
-terraform import sumologic_http_source.kube_scheduler_metrics_source "$COLLECTOR_NAME/kube-scheduler-metrics"
-terraform import sumologic_http_source.kube_state_metrics_source "$COLLECTOR_NAME/kube-state-metrics"
-terraform import sumologic_http_source.kubelet_metrics_source "$COLLECTOR_NAME/kubelet-metrics"
-terraform import sumologic_http_source.logs_source "$COLLECTOR_NAME/logs"
-terraform import sumologic_http_source.node_exporter_metrics_source "$COLLECTOR_NAME/node-exporter-metrics"
+{{ range $source := .Values.sumologic.sources }}
+terraform import sumologic_http_source.{{ template "sources.terraform_name" $source }} "$COLLECTOR_NAME/{{ $source.value }}"
+{{- end }}
 
 # Kubernetes Namespace and Secret
 terraform import kubernetes_namespace.sumologic_collection_namespace {{ .Release.Namespace }}
