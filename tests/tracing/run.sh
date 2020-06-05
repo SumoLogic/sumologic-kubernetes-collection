9b3bb1ba3432cff8ba6635740c373e253fedc30f
Przemek Maciolek <pmaciolek@gmail.com>
2020-06-05 19:46:09 +0200

    Add trace template tests

---
+++ tests/tracing/run.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+test_start()        { echo -e "[.] $*"; }
+test_passed()       { echo -e "[+] $*"; }
+test_failed()       { echo -e "[-] $*"; }
+
+readonly SCRIPT_PATH="$( dirname $(realpath ${0}) )"
+readonly STATICS_PATH="${SCRIPT_PATH}/static"
+readonly INPUT_FILES="$(ls "${STATICS_PATH}" | grep input)"
+readonly OUT="new_values.yaml"
+
+#docker run --rm \
+#  -v ${SCRIPT_PATH}/../../deploy/helm/sumologic:/chart \
+#  sumologic/kubernetes-tools:master \
+#  helm dependency update /chart
+
+for input_file in ${INPUT_FILES}; do
+  test_name=$(echo "${input_file}" | sed -e 's/.input.yaml$//g')
+  output_file="${test_name}.output.yaml"
+
+  test_start "${test_name}" ${input_file}
+  docker run --rm \
+    -v ${SCRIPT_PATH}/../../deploy/helm/sumologic:/chart \
+    -v "${STATICS_PATH}/${input_file}":/values.yaml \
+    sumologic/kubernetes-tools:master \
+    helm template /chart -f /values.yaml \
+      --namespace sumologic \
+      --set sumologic.traces.enabled=true \
+      --set sumologic.accessId='accessId' \
+      --set sumologic.accessKey='accessKey' \
+      -s templates/otelcol-configmap.yaml 2>/dev/null 1> "${OUT}"
+
+  test_output=$(diff "${STATICS_PATH}/${output_file}" "${OUT}" | cat -te)
+
+  if [[ -n "${test_output}" ]]; then
+    echo -e "\tOutput diff (${STATICS_PATH}/${output_file}):\n${test_output}"
+    test_failed "${test_name}"
+  else
+    test_passed "${test_name}"
+  fi
+
+  rm "${OUT}"
+done
