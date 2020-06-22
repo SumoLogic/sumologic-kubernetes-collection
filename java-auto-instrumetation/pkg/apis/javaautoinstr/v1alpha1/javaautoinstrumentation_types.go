7a707ea94402b449d1d09c69412fe2d3cb38767f
Piotr Glazar <pglazar@sumologic.com>
2020-06-22 14:57:58 +0000

    Adding a field to CRD

--- java-auto-instrumetation/pkg/apis/javaautoinstr/v1alpha1/javaautoinstrumentation_types.go
+++ java-auto-instrumetation/pkg/apis/javaautoinstr/v1alpha1/javaautoinstrumentation_types.go
@@ -12,6 +12,8 @@ type JavaAutoInstrumentationSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// Currently a placeholder
+	JavaOptions string `json:"javaOptions"`
 }
 
 // JavaAutoInstrumentationStatus defines the observed state of JavaAutoInstrumentation
@@ -19,6 +21,8 @@ type JavaAutoInstrumentationStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	// Currently a placeholder
+	JavaOptions string `json:"javaOptions"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
