406eefccc10bab9026ad28e276afc7b4a90196cc
Frank Reno <frank.reno@me.com>
2020-06-29 13:57:47 -0600

    update CI

--- ci/build.sh
+++ ci/build.sh
@@ -76,6 +76,7 @@ if [ -n "$GITHUB_TOKEN" ] && [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
   echo "# This file is auto-generated." > deploy/kubernetes/fluentd-sumologic.yaml.tmpl
   sudo helm init --client-only
   sudo helm repo add falcosecurity https://falcosecurity.github.io/charts
+  sudo helm repo add fluent https://fluent.github.io/helm-charts
   cd deploy/helm/sumologic
   sudo helm dependency update
   cd ../../../
@@ -170,6 +171,7 @@ function push_helm_chart() {
   git checkout -- .
   sudo helm init --client-only
   sudo helm repo add falcosecurity https://falcosecurity.github.io/charts
+  sudo helm repo add fluent https://fluent.github.io/helm-charts
   sudo helm package deploy/helm/sumologic --dependency-update --version=$version --app-version=$version
   git fetch origin-repo
   git checkout gh-pages
