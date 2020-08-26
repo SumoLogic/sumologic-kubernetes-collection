2d788743859c6d052bcf5273560e3dc917c2960d
Sam Song <ssong@sumologic.com>
2020-08-26 08:21:15 -0700

    attach node metadata to metrics when not present

--- fluent-plugin-enhance-k8s-metadata/lib/sumologic/kubernetes/reader.rb
+++ fluent-plugin-enhance-k8s-metadata/lib/sumologic/kubernetes/reader.rb
@@ -24,6 +24,9 @@ module SumoLogic
         labels = pod['metadata']['labels']
         metadata['pod_labels'] = {'pod_labels' => labels} if labels.is_a?(Hash)
 
+        node = pod['spec']['nodeName']
+        metadata['node'] = node
+
         owners = fetch_pod_owners(namespace, pod)
         metadata['owners'] = owners if owners.is_a?(Hash)
         metadata
