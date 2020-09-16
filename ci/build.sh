bd9e6839b96f9c851c0397960449d58dd0b58832
Marcin 'Perk' Stożek <perk@sumologic.com>
2020-09-16 14:57:32 +0200

    fix: ci/build shellcheck SC2086 (double quotes)
    Fix for the following shellcheck rule
    SC2086: Double quote to prevent globbing and word splitting

--- ci/build.sh
+++ ci/build.sh
@@ -17,9 +17,9 @@ trap 'err_report $LINENO' ERR
 if [ -n "$GITHUB_TOKEN" ]; then
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  git remote add origin-repo https://${GITHUB_TOKEN}@github.com/SumoLogic/sumologic-kubernetes-collection.git > /dev/null 2>&1
+  git remote add origin-repo "https://${GITHUB_TOKEN}@github.com/SumoLogic/sumologic-kubernetes-collection.git" > /dev/null 2>&1
   git fetch --unshallow origin-repo
-  git checkout $TRAVIS_PULL_REQUEST_BRANCH
+  git checkout "$TRAVIS_PULL_REQUEST_BRANCH"
 fi
 
 ## check the build script with shellcheck
@@ -32,7 +32,7 @@ shellcheck ci/build.sh || true
 if [ -n "$GITHUB_TOKEN" ] && [ "$TRAVIS_EVENT_TYPE" == "pull_request" ] && [[ ! "$TRAVIS_PULL_REQUEST_BRANCH" =~ ^revert- ]]; then
   # Check most recent commit author. If non-Travis, check for changes made to generated files
   recent_author=$(git log origin-repo/master..HEAD --format="%an" | grep -m1 "")
-  if echo $recent_author | grep -v -q -i "travis"; then
+  if echo "$recent_author" | grep -v -q -i "travis"; then
     # NOTE(ryan, 2019-08-30): Append "|| true" to command to ignore non-zero exit code
     changes=$(git log origin-repo/master..HEAD --name-only --format="" --author="$recent_author" | grep -i "fluentd-sumologic.yaml.tmpl\|fluent-bit-overrides.yaml\|prometheus-overrides.yaml\|falco-overrides.yaml") || true
     if [ -n "$changes" ]; then
@@ -44,13 +44,13 @@ fi
 
 for i in ./fluent-plugin* ; do
   if [ -d "$i" ]; then
-    cd $i
+    cd "$i"
     PLUGIN_NAME=$(basename "$i")
     # Strip everything after "-" (longest match) to avoid gem prerelease behavior
     GEM_VERSION=${VERSION%%-*}
     echo "Building gem $PLUGIN_NAME version $GEM_VERSION in $(pwd) ..."
-    sed -i.bak "s/0.0.0/$GEM_VERSION/g" ./$PLUGIN_NAME.gemspec
-    rm -f ./$PLUGIN_NAME.gemspec.bak
+    sed -i.bak "s/0.0.0/$GEM_VERSION/g" "./$PLUGIN_NAME.gemspec"
+    rm -f "./$PLUGIN_NAME.gemspec.bak"
 
     echo "Install bundler..."
     bundle install
@@ -59,7 +59,7 @@ for i in ./fluent-plugin* ; do
     bundle exec rake
 
     echo "Build gem $PLUGIN_NAME $GEM_VERSION..."
-    gem build $PLUGIN_NAME
+    gem build "$PLUGIN_NAME"
     mv *.gem ../deploy/docker/gems
 
     cd ..
@@ -68,7 +68,7 @@ done
 
 echo "Building docker image with $DOCKER_TAG:local in $(pwd)..."
 cd ./deploy/docker
-docker build . -f ./Dockerfile -t $DOCKER_TAG:local --no-cache
+docker build . -f ./Dockerfile -t "$DOCKER_TAG:local" --no-cache
 rm -f ./gems/*.gem
 cd ../..
 
@@ -192,10 +192,10 @@ function push_docker_image() {
   local version="$1"
 
   echo "Tagging docker image $DOCKER_TAG:local with $DOCKER_TAG:$version..."
-  docker tag $DOCKER_TAG:local $DOCKER_TAG:$version
+  docker tag "$DOCKER_TAG:local" "$DOCKER_TAG:$version"
   echo "Pushing docker image $DOCKER_TAG:$version..."
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  docker push $DOCKER_TAG:$version
+  docker push "$DOCKER_TAG:$version"
 }
 
 function push_helm_chart() {
@@ -207,7 +207,7 @@ function push_helm_chart() {
   sudo helm init --client-only
   sudo helm repo add falcosecurity https://falcosecurity.github.io/charts
   sudo helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-  sudo helm package deploy/helm/sumologic --dependency-update --version=$version --app-version=$version
+  sudo helm package deploy/helm/sumologic --dependency-update --version="$version" --app-version="$version"
   git fetch origin-repo
   git checkout gh-pages
   sudo helm repo index ./ --url https://sumologic.github.io/sumologic-kubernetes-collection/
