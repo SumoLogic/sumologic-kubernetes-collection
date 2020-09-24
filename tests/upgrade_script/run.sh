fb876ef66a64c75feb8c5f07d0528e3401ff5547
Dominik Rosiek <drosiek@sumologic.com>
2020-09-24 11:13:05 +0200

    Fix upgrade_script tests

--- tests/upgrade_script/run.sh
+++ tests/upgrade_script/run.sh
@@ -14,30 +14,31 @@ test_failed()       { echo -e "[-] $*"; }
 SCRIPT_PATH="$( dirname $(realpath ${0}) )"
 
 source "${SCRIPT_PATH}/../functions.sh"
-readonly TMP_OUT="tmp/out.log"
+readonly TEST_TMP_OUT="tmp/out.log"
 
-get_variables "${SCRIPT_PATH}"
-prepare_environment
+set_variables "${SCRIPT_PATH}"
+prepare_tests
 
 SUCCESS=0
-for input_file in ${INPUT_FILES}; do
+for input_file in ${TEST_INPUT_FILES}; do
   test_name=$(echo "${input_file}" | sed -e 's/.input.yaml$//g')
   output_file="${test_name}.output.yaml"
   log_file="${test_name}.log"
 
   test_start "${test_name}" ${input_file}
-  bash "${SCRIPT_PATH}/../../deploy/helm/sumologic/upgrade-1.0.0.sh" "${STATICS_PATH}/${input_file}" 1>"${TMP_OUT}" 2>&1
+  bash "${TEST_SCRIPT_PATH}/../../deploy/helm/sumologic/upgrade-1.0.0.sh" "${TEST_STATICS_PATH}/${input_file}" 1>"${TEST_TMP_OUT}" 2>&1
+  mv new_values.yaml "${TEST_OUT}"
 
-  test_output=$(diff "${STATICS_PATH}/${output_file}" "${OUT}")
-  test_log=$(diff "${STATICS_PATH}/${log_file}" "${TMP_OUT}")
-  rm "${TMP_OUT}" "${OUT}"
+  test_output=$(diff "${TEST_STATICS_PATH}/${output_file}" "${TEST_OUT}")
+  test_log=$(diff "${TEST_STATICS_PATH}/${log_file}" "${TEST_TMP_OUT}")
+  rm "${TEST_TMP_OUT}" "${TEST_OUT}"
 
   if [[ -n "${test_output}" || -n "${test_log}" ]]; then
     if [[ -n "${test_output}" ]]; then
-      echo -e "\tOutput diff (${STATICS_PATH}/${output_file}):\n${test_output}"
+      echo -e "\tOutput diff (${TEST_STATICS_PATH}/${output_file}):\n${test_output}"
     fi
     if [[ -n "${test_log}" ]]; then
-      echo -e "\tLog diff (${STATICS_PATH}/${log_file}):\n${test_log}"
+      echo -e "\tLog diff (${TEST_STATICS_PATH}/${log_file}):\n${test_log}"
     fi
     test_failed "${test_name}"
     SUCCESS=1
@@ -46,6 +47,6 @@ for input_file in ${INPUT_FILES}; do
   fi
 done
 
-cleanup_environment
+cleanup_tests
 
 exit $SUCCESS
