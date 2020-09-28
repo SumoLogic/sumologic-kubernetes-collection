d669588537d2244d410a04f40b7af4749b1ecbf6
Andrzej Stencel <astencel@sumologic.com>
2020-09-28 15:30:08 +0200

    Replace sed with yq

--- vagrant/scripts/generate-local-config.sh
+++ vagrant/scripts/generate-local-config.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-cat > /sumologic/vagrant/values.local.yaml <<END
-image:
-  repository: localhost:32000/sumologic/kubernetes-fluentd
-  tag: local
-  pullPolicy: Always
-END
+touch /sumologic/vagrant/values.local.yaml
