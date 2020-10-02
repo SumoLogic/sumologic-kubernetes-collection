a173f72120ce1dc98f1978ab3a747a66940437f6
Vijit Singhal <56007827+vsinghal13@users.noreply.github.com>
2020-10-02 15:28:02 -0700

    Revert "Revert to using helm stable for Prometheus Operator chart" (#979)
    This reverts commit 12916f9c93c61dec6ba4856a5c757e8f8af5e611.

--- ci/build.sh
+++ ci/build.sh
@@ -123,6 +123,7 @@ if [ -n "$GITHUB_TOKEN" ] && [ "$TRAVIS_EVENT_TYPE" == "pull_request" ]; then
   echo "# This file is auto-generated." > deploy/kubernetes/fluentd-sumologic.yaml.tmpl
   sudo helm init --client-only
   sudo helm repo add falcosecurity https://falcosecurity.github.io/charts
+  sudo helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
   cd deploy/helm/sumologic || exit 1
   sudo helm dependency update
   cd ../../../ || exit 1
@@ -217,6 +218,7 @@ function push_helm_chart() {
   git checkout -- .
   sudo helm init --client-only
   sudo helm repo add falcosecurity https://falcosecurity.github.io/charts
+  sudo helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
   sudo helm package deploy/helm/sumologic --dependency-update --version="$version" --app-version="$version"
   git fetch origin-repo
   git checkout gh-pages
