a6b380a84103cd22b6420ace9767a44024f60303
Dominik Rosiek <drosiek@sumologic.com>
2020-10-19 21:43:01 +0200

    Add support for custom terraform and bash scripts to the setup

---
+++ deploy/helm/sumologic/conf/setup/custom.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# This script copies files from /customer-scripts to /scripts/<dirname> basing on the filename
+#
+# Example file structure:
+#
+# /customer-scripts
+# ├── dir1_main.tf
+# ├── dir1_setup.sh
+# ├── dir2_list.txt
+# └── dir2_setup.sh
+#
+# Expected structure:
+#
+# /scripts
+# ├── dir1
+# │   ├── main.tf
+# │   └── setup.sh
+# └── dir2
+#     ├── list.txt
+#     └── setup.sh
+#
+# shellcheck disable=SC2010
+# extract target directory names from the file names using _ as separator
+for dir in $(ls -1 /customer-scripts | grep _ | grep -oE '^.*?_' | sed 's/_//g' | sort | uniq); do
+  target="/scripts/${dir}"
+  mkdir "${target}"
+  # shellcheck disable=SC2010
+  # Get files for given directory and take only filename part (after first _)
+  for file in $(ls -1 "/customer-scripts/${dir}_"* | grep -oE '_.*' | sed 's/_//g'); do
+    cp "/customer-scripts/${dir}_${file}" "${target}/${file}"
+  done
+  cd "${target}" && bash setup.sh
+done
