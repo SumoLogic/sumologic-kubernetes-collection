cfeba99edbf478a24f3dd719225f16adb23a121a
Patryk Małek <pmalek@sumologic.com>
2020-10-22 18:15:49 +0200

    Small tweaks and Makefile changes

--- vagrant/scripts/perf-test.sh
+++ vagrant/scripts/perf-test.sh
@@ -3,6 +3,7 @@
 set -e
 
 # Exemplar script output
+# TEST_DURATION=300 TEST_WARMUP_DURATION=30 ./scripts/perf-test.sh
 #
 # Starting perf tests
 # Setting up avalanche...
@@ -14,6 +15,8 @@ set -e
 # --metric-interval=60000
 # --value-interval=30
 #
+# Letting cluster warmup for 10s...
+# Resetting metrics count for receiver-mock-8659b9b57-x975t
 # Letting prometheus ingest metrics for 20s...
 # Cleaning up avalanche...
 #
@@ -25,7 +28,9 @@ set -e
 
 readonly DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 readonly AVALANCHE_YAML_PATH="${DIR}/../k8s/avalanche.yaml"
-readonly RECEIVER_MOCK_POD="$(kubectl get pod -n receiver-mock --no-headers -lapp=receiver-mock --output custom-columns=NAME:.metadata.name | head -1)"
+readonly RECEIVER_MOCK_PODS="$(kubectl get pod -n receiver-mock --no-headers -lapp=receiver-mock --output custom-columns=NAME:.metadata.name)"
+readonly RECEIVER_MOCK_POD="$(kubectl get pod -n receiver-mock --no-headers -lapp=receiver-mock -o=jsonpath='{.items[0].metadata.name}')"
+readonly PROMETHEUS_API_QUERY_URL="collection-prometheus-oper-prometheus.sumologic.svc.cluster.local:9090/api/v1/query"
 
 function is_avalanche_running() {
     local out
@@ -41,23 +46,29 @@ function container_memory_rss() {
     local container_name="${1}"
 
     kubectl exec -n receiver-mock "${RECEIVER_MOCK_POD}" \
-        -- curl -s -g -XPOST collection-prometheus-oper-prometheus.sumologic.svc.cluster.local:9090/api/v1/query \
+        -- curl -s -g -XPOST "${PROMETHEUS_API_QUERY_URL}" \
         --data-urlencode "query=container_memory_rss{job='kubelet',container='${container_name}'}" \
-        | jq -r '.data.result[0].value[1]'
+        | jq -r '.data.result[0].value[1]' # value[0] is the timestamp
 }
 
 function ingested_metrics_count() {
-    kubectl exec -n receiver-mock "${RECEIVER_MOCK_POD}" -- curl -s -XGET localhost:3000/metrics | \
-        grep "^receiver_mock_metrics_count" | awk '{print $2}'
+    kubectl exec -n receiver-mock "${RECEIVER_MOCK_POD}" \
+        -- curl -s -g -XPOST "${PROMETHEUS_API_QUERY_URL}" \
+        --data-urlencode "query=sum(receiver_mock_metrics_count)" \
+        | jq -r '.data.result[0].value[1]' # value[0] is the timestamp
 }
 
 function reset_receiver_mock_metrics() {
-    kubectl exec -n receiver-mock "${RECEIVER_MOCK_POD}" \
-        -- curl -s -XPOST localhost:3000/metrics-reset >/dev/null
+    for pod in ${RECEIVER_MOCK_PODS}; do
+        printf "Resetting metrics count for %s\n" "${pod}"
+        kubectl exec -n receiver-mock "${pod}" \
+            -- curl -s -XPOST localhost:3000/metrics-reset >/dev/null
+    done
 }
 
 function run_test() {
-    local duration_seconds="${1}"
+    local WARMUP_DURATION_SECONDS="${1}"
+    local DURATION_SECONDS="${2}"
     echo "Starting perf tests"
 
     if [[ $(is_avalanche_running) -eq 0 ]]; then
@@ -70,8 +81,6 @@ function run_test() {
     local AVALANCHE_COUNT
     local AVALANCHE_POD_NAME
 
-    PROMETHEUS_MEMORY_RSS_START=$(container_memory_rss prometheus)
-
     echo "Setting up avalanche..."
     kubectl apply --wait -f "${AVALANCHE_YAML_PATH}" >/dev/null || \
         (echo "Failed applying avalanche yaml (${AVALANCHE_YAML_PATH})" && exit 1)
@@ -83,8 +92,13 @@ function run_test() {
     kubectl get pod "${AVALANCHE_POD_NAME}" -n avalanche -o json | jq -r '.spec.containers[0].args[]'
     echo
 
-    echo "Letting prometheus ingest metrics for ${duration_seconds}s..."
-    sleep "${duration_seconds}"
+    echo "Letting cluster warmup for ${WARMUP_DURATION_SECONDS}s..."
+    sleep "${WARMUP_DURATION_SECONDS}"
+
+    reset_receiver_mock_metrics
+    PROMETHEUS_MEMORY_RSS_START=$(container_memory_rss prometheus)
+    echo "Letting prometheus ingest metrics for ${DURATION_SECONDS}s..."
+    sleep "${DURATION_SECONDS}"
     PROMETHEUS_MEMORY_RSS_FINISH=$(container_memory_rss prometheus)
 
     echo "Cleaning up avalanche..."
@@ -100,23 +114,28 @@ function run_test() {
 
 function usage() {
     echo "Usage:"
-    echo "${0} <test_duration_in_seconds>"
+    echo "TEST_DURATION=<duration_in_seconds> TEST_WARMUP_DURATION=<warmup_in_seconds> ${0}"
 }
 
-readonly DURATION="${1}"
-if [[ -z "${DURATION}" ]] ; then
-    usage
-    exit 1
-fi
+function check_duration_variable() {
+    local VARIABLE_NAME="${1}"
+    if [[ -z "${!VARIABLE_NAME}" ]] ; then
+        usage
+        exit 1
+    fi
+    if ! grep -E "^[0-9]+$" <<< "${!VARIABLE_NAME}" >/dev/null; then
+        usage
+        echo
+        echo "Failed to parse duration \"${!VARIABLE_NAME}\""
+        exit 1
+    fi
+}
 
-if ! grep -E "^[0-9]+$" <<< "${DURATION}" >/dev/null; then
-    usage
-    echo
-    echo "Failed to parse duration \"${DURATION}\""
-    exit 1
-fi
+readonly TEST_DURATION="${TEST_DURATION}"
+readonly TEST_WARMUP_DURATION="${TEST_WARMUP_DURATION}"
+check_duration_variable TEST_DURATION
+check_duration_variable TEST_WARMUP_DURATION
 
-reset_receiver_mock_metrics
-run_test "${DURATION}"
+run_test "${TEST_WARMUP_DURATION}" "${TEST_DURATION}"
 readonly metric_count="$(ingested_metrics_count)"
 printf "receiver-mock ingested %d metrics during the test\n" "${metric_count}"
