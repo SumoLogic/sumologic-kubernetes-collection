47b28941e1f5c187d68ccd0a016525f67f060b9c
Dominik Rosiek <drosiek@sumologic.com>
2020-10-29 14:18:55 +0100

    Stop generating override files

--- ci/build.sh
+++ ci/build.sh
@@ -121,22 +121,6 @@ function push_helm_chart() {
   set +x
 }
 
-function validate_changes_to_generated_files() {
-  local changes
-  local recent_author
-
-  # Check most recent commit author. If non-Travis, check for changes made to generated files
-  recent_author=$(git log origin-repo/master..HEAD --format="%an" | grep -m1 "")
-  if echo "${recent_author}" | grep -v -q -i "travis"; then
-    # NOTE(ryan, 2019-08-30): Append "|| true" to command to ignore non-zero exit code
-    changes=$(git log origin-repo/master..HEAD --name-only --format="" --author="${recent_author}" | grep -i "fluentd-sumologic.yaml.tmpl\|fluent-bit-overrides.yaml\|prometheus-overrides.yaml\|falco-overrides.yaml") || true
-    if [ -n "${changes}" ]; then
-      echo "Aborting due to manual changes detected in the following generated files: ${changes}"
-      exit 1
-    fi
-  fi
-}
-
 function build_docker_image() {
   local tag
   local no_cache
@@ -156,129 +140,13 @@ function build_docker_image() {
   ruby deploy/test/test_docker.rb || exit 1
 }
 
-function generate_overrides() {
-  local with_files
-  local prometheus_remote_write
-  local branch
-
-  branch="${1}"
-
-  echo "Generating deployment yaml from helm chart..."
-  echo "# This file is auto-generated." > deploy/kubernetes/fluentd-sumologic.yaml.tmpl
-  helm dependency update deploy/helm/sumologic
-
-  # Get list of templates available for default values.yaml
-  with_files=$(find deploy/helm/sumologic/templates -maxdepth 1 -iname "*.yaml" ! -path "*/otelcol-*" ! -path "*/scc.yaml" ! -path "*/*hpa.yaml" ! -path "*/*psp.yaml" | sed 's#deploy/helm/sumologic/templates#-s templates#g' | sed 's/yaml/yaml /g')
-  # shellcheck disable=SC2086
-  helm template deploy/helm/sumologic \
-    ${with_files} \
-    --namespace "\$NAMESPACE" \
-    --name-template collection \
-    --set dryRun=true >> deploy/kubernetes/fluentd-sumologic.yaml.tmpl 2>/dev/null \
-    --set sumologic.endpoint="bogus" \
-    --set sumologic.accessId="bogus" \
-    --set sumologic.accessKey="bogus"
-
-  if [[ -n $(git diff deploy/kubernetes/fluentd-sumologic.yaml.tmpl) ]]; then
-      echo "Detected changes in 'fluentd-sumologic.yaml.tmpl', committing the updated version to ${branch}..."
-      git add deploy/kubernetes/fluentd-sumologic.yaml.tmpl
-      git commit -m "Generate new 'fluentd-sumologic.yaml.tmpl'"
-      git push --quiet origin-repo "${branch}"
-  else
-      echo "No changes in 'fluentd-sumologic.yaml.tmpl'."
-  fi
-
-  echo "Generating setup job yaml from helm chart..."
-  echo "# This file is auto-generated." > deploy/kubernetes/setup-sumologic.yaml.tmpl
-
-  # Get list of templates available for default values.yaml
-  with_files=$(find deploy/helm/sumologic/templates/setup -maxdepth 1 -iname "*.yaml" ! -path "*/setup-scc.yaml" | sed 's#deploy/helm/sumologic/templates#-s templates#g' | sed 's/yaml/yaml /g')
-  # shellcheck disable=SC2086
-  helm template deploy/helm/sumologic \
-    ${with_files} \
-    --namespace "\$NAMESPACE" \
-    --name-template collection \
-    --set dryRun=true >> deploy/kubernetes/setup-sumologic.yaml.tmpl 2>/dev/null \
-    --set sumologic.accessId="\$SUMOLOGIC_ACCESSID" \
-    --set sumologic.accessKey="\$SUMOLOGIC_ACCESSKEY" \
-    --set sumologic.collectorName="\$COLLECTOR_NAME" \
-    --set sumologic.clusterName="\$CLUSTER_NAME"
-
-  if [[ -n $(git diff deploy/kubernetes/setup-sumologic.yaml.tmpl) ]]; then
-      echo "Detected changes in 'setup-sumologic.yaml.tmpl', committing the updated version to ${branch}..."
-      git add deploy/kubernetes/setup-sumologic.yaml.tmpl
-      git commit -m "Generate new 'setup-sumologic.yaml.tmpl'"
-      git push --quiet origin-repo "${branch}"
-  else
-      echo "No changes in 'setup-sumologic.yaml.tmpl'."
-  fi
-
-  # Generate override yaml files for chart dependencies to determine if changes are made to overrides yaml files
-  echo "Generating overrides files..."
-  
-  echo "Copy metrics-server section from 'values.yaml'  to 'metrics-server-overrides.yaml'"
-  echo "# This file is auto-generated." > deploy/helm/metrics-server-overrides.yaml
-  # Copy lines of metrics_server section and remove indention from values.yaml
-  yq r deploy/helm/sumologic/values.yaml metrics-server | yq d - enabled >> deploy/helm/metrics-server-overrides.yaml
-  
-  echo "Copy  fluent-bit section from 'values.yaml' to 'fluent-bit-overrides.yaml'"
-  echo "# This file is auto-generated." > deploy/helm/fluent-bit-overrides.yaml
-  # Copy lines of fluent-bit section and remove indention from values.yaml
-  yq r deploy/helm/sumologic/values.yaml fluent-bit | yq d - enabled >> deploy/helm/fluent-bit-overrides.yaml
-  
-  echo "Copy prometheus-operator section from 'values.yaml' to  'prometheus-overrides.yaml'"
-  echo "# This file is auto-generated." > deploy/helm/prometheus-overrides.yaml
-  # Copy lines of prometheus-operator section and remove indention from values.yaml
-  yq r deploy/helm/sumologic/values.yaml prometheus-operator >> deploy/helm/prometheus-overrides.yaml
-
-  echo "Copy prometheus.prometheusSpec.remoteWrite from 'prometheus-overrides.yaml' and inject into 'deploy/kubernetes/kube-prometheus-sumo-logic-mixin.libsonnet'"
-  prometheus_remote_write=$(yq r deploy/helm/prometheus-overrides.yaml prometheus.prometheusSpec.remoteWrite -j | jq '.' | sed 's/^/    /')
-  # Escaping so sed will work
-  prometheus_remote_write="${prometheus_remote_write//\\/\\\\}"
-  prometheus_remote_write="${prometheus_remote_write//\//\\/}"
-  prometheus_remote_write="${prometheus_remote_write//&/\\&}"
-  prometheus_remote_write="${prometheus_remote_write//$'\n'/\\n}"
-  echo "// This file is autogenerated" > deploy/kubernetes/kube-prometheus-sumo-logic-mixin.libsonnet
-  # shellcheck disable=SC2016
-  sed "s#\[\/\*REMOTE_WRITE\*\/\]#${prometheus_remote_write}#" ci/jsonnet-mixin.tmpl \
-    | sed 's#"http://$(CHART).$(NAMESPACE).svc.cluster.local:9888\/#$._config.sumologicCollectorSvc + "#g' \
-    | sed 's/+:     /+: /' \
-    | sed -r 's/"(\w*)":/\1:/g' > deploy/kubernetes/kube-prometheus-sumo-logic-mixin.libsonnet
-  
-  echo "Copy falco section from 'values.yaml' to 'falco-overrides.yaml'"
-  echo "# This file is auto-generated." > deploy/helm/falco-overrides.yaml
-  # Copy lines of falco section and remove indention from values.yaml
-  yq r deploy/helm/sumologic/values.yaml falco | yq d - enabled >> deploy/helm/falco-overrides.yaml
-
-  if [ -n "$(git diff deploy/helm/metrics-server-overrides.yaml)" ] || [ -n "$(git diff deploy/helm/fluent-bit-overrides.yaml)" ] || [ -n "$(git diff deploy/helm/prometheus-overrides.yaml)" ] || [ -n "$(git diff deploy/helm/falco-overrides.yaml)" ] || [ -n "$(git diff deploy/kubernetes/kube-prometheus-sumo-logic-mixin.libsonnet)" ]; then
-    echo "Detected changes in 'fluent-bit-overrides.yaml', 'prometheus-overrides.yaml', 'falco-overrides.yaml', or 'kube-prometheus-sumo-logic-mixin.libsonnet', committing the updated version to ${TRAVIS_PULL_REQUEST_BRANCH}..."
-    git add deploy/helm/*-overrides.yaml
-    git add deploy/kubernetes/kube-prometheus-sumo-logic-mixin.libsonnet
-    git commit -m "Generate new overrides yaml/libsonnet file(s)."
-    git push --quiet origin-repo "${branch}"
-  else
-    echo "No changes in the generated overrides files."
-  fi
-}
-
 # Set up Github
 if [ -n "${GITHUB_TOKEN}" ]; then
   set_up_github "${GITHUB_TOKEN}" "$(get_branch_to_checkout)"
 fi
 
-# Check for invalid changes to generated yaml files (non-Tag builds)
-# Exclude branches that start with "revert-" to allow reverts
-if [ -n "${GITHUB_TOKEN}" ] && [ "${TRAVIS_EVENT_TYPE}" == "pull_request" ] && [[ ! "${TRAVIS_PULL_REQUEST_BRANCH}" =~ ^revert- ]]; then
-  validate_changes_to_generated_files || (echo "Aborting due to manual changes detected in generated files" && exit 1)
-fi
-
 bundle_fluentd_plugins "${VERSION}" || (echo "Failed bundling fluentd plugins" && exit 1)
 build_docker_image "${DOCKER_TAG}" || (echo "Error during building docker image" && exit 1)
-
-# Check for changes that require re-generating overrides yaml files
-if [ -n "${GITHUB_TOKEN}" ] && [ "${TRAVIS_EVENT_TYPE}" == "pull_request" ]; then
-  generate_overrides "${TRAVIS_PULL_REQUEST_BRANCH}" || (echo "Error generating override files" && exit 1)
-fi
 
 if [ -n "${DOCKER_PASSWORD}" ] && [ -n "${TRAVIS_TAG}" ]; then
   push_docker_image "${VERSION}"
