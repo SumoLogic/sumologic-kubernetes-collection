46fc3a72a3e09b91e04a5f5d84712d042ce0af97
Patryk Małek <69143962+pmalek-sumo@users.noreply.github.com>
2020-11-30 11:01:20 +0100

    Add Github Actions build and push actions (#1186)

--- ci/tests.sh
+++ ci/tests.sh
@@ -2,57 +2,17 @@
 
 set -e
 
-VERSION="${TRAVIS_TAG:-0.0.0}"
-VERSION="${VERSION#v}"
-
 readonly ROOT_DIR="$(dirname "$(dirname "${0}")")"
-cd "${ROOT_DIR}"
+# shellcheck disable=SC1090
+source "${ROOT_DIR}"/ci/_test_functions.sh
+# shellcheck disable=SC1090
+source "${ROOT_DIR}"/ci/_build_functions.sh
 
-function test_fluentd_plugin() {
-  local plugin_name="${1}"
-  local version="${2}"
-  # Strip everything after "-" (longest match) to avoid gem prerelease behavior
-  local gem_version="${version%%-*}"
-  local result
+fetch_current_branch
+VERSION="$(git describe --tags)"
+readonly VERSION="${VERSION#v}"
 
-  pushd "${plugin_name}" || exit 1
-
-  if [[ -z "${version}" ]] ; then
-    echo "Please provide the version when bundling fluentd plugins"
-    exit 1
-  fi
-
-  echo "Preparing gem ${plugin_name} version ${gem_version} in $(pwd) for testing ..."
-  sed -i.bak "s/0.0.0/${gem_version}/g" ./"${plugin_name}".gemspec
-  rm -f ./"${plugin_name}".gemspec.bak
-
-  echo "Install bundler..."
-  bundle install
-
-  echo "Run unit tests..."
-  bundle exec rake
-
-  readonly result=$?
-  popd || exit 1
-  if [ "${result}" -ne "0" ]; then
-   exit 1
-  fi
-}
-
-function test_fluentd_plugins() {
-  local version="${1}"
-
-  if [[ -z "${version}" ]] ; then
-    echo "Please provide the version when bundling fluentd plugins"
-    exit 1
-  fi
-
-  find . -maxdepth 1 -name 'fluent-plugin-*' -type 'd' -print |
-    while read -r line; do
-      # Run tests in their own context
-      test_fluentd_plugin "$(basename "${line}")" "${version}" || exit 1
-    done
-}
+pushd "${ROOT_DIR}" || exit 1
 
 # Test if template files are generated correctly for various values.yaml
 echo "Test helm templates generation"
@@ -68,5 +28,7 @@ echo "Test upgrade v2 script..."
 
 # Test fluentd plugins
 test_fluentd_plugins "${VERSION}" || (echo "Failed testing fluentd plugins" && exit 1)
+
+popd || exit 1
 
 echo "DONE"
