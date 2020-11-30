9165b24e9e959daee722e9f99b5628b69dd4f874
Patryk Małek <69143962+pmalek-sumo@users.noreply.github.com>
2020-11-30 12:55:06 +0100

    Backport CI migration to Github actions (#1192)

---
+++ ci/tests.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly ROOT_DIR="$(dirname "$(dirname "${0}")")"
+# shellcheck disable=SC1090
+source "${ROOT_DIR}"/ci/_test_functions.sh
+# shellcheck disable=SC1090
+source "${ROOT_DIR}"/ci/_build_functions.sh
+
+fetch_current_branch
+VERSION="$(git describe --tags)"
+readonly VERSION="${VERSION#v}"
+
+pushd "${ROOT_DIR}" || exit 1
+
+# Test if template files are generated correctly for various values.yaml
+echo "Test helm templates generation"
+./tests/run.sh || (echo "Failed testing templates" && exit 1)
+
+# Test upgrade script
+echo "Test upgrade script..."
+./tests/upgrade_script/run.sh || (echo "Failed testing upgrade script" && exit 1)
+
+# Test fluentd plugins
+test_fluentd_plugins "${VERSION}" || (echo "Failed testing fluentd plugins" && exit 1)
+
+popd || exit 1
+
+echo "DONE"
