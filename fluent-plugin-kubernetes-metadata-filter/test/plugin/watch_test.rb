8053e2952d2e43652811cdf39f9c2bec639c327b
Andrzej Stencel <astencel@sumologic.com>
2020-12-14 16:17:59 +0100

    Bundle kubernetes_metadata plugin with fix for Fluentd pod restarts (#1183) (#1193)
    This change bundles the `kubernetes_metadata` Fluentd plugin from https://github.com/fabric8io/fluent-plugin-kubernetes_metadata_filter/ into our repo, as the upstream is not active and our [pull request there](https://github.com/fabric8io/fluent-plugin-kubernetes_metadata_filter/pull/267) is not being merged despite being accepted.
    
    This code was copied from upstream at commit https://github.com/fabric8io/fluent-plugin-kubernetes_metadata_filter/commit/84f66a8f9e06ab5b5211053fcce4cd8ab4bd74ba on `master` branch from Oct 8th, 2020. On that commit, changes from the pull request https://github.com/fabric8io/fluent-plugin-kubernetes_metadata_filter/pull/267 have been applied.

---
+++ fluent-plugin-kubernetes-metadata-filter/test/plugin/watch_test.rb
@@ -0,0 +1,68 @@
+#
+# Fluentd Kubernetes Metadata Filter Plugin - Enrich Fluentd events with
+# Kubernetes metadata
+#
+# Copyright 2015 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require_relative '../helper'
+
+class WatchTest < Test::Unit::TestCase
+
+  def thread_current_running?
+    true
+  end
+
+  setup do
+    @annotations_regexps = []
+    @namespace_cache = {}
+    @watch_retry_max_times = 2
+    @watch_retry_interval = 1
+    @watch_retry_exponential_backoff_base = 2
+    @cache = {}
+    @stats = KubernetesMetadata::Stats.new
+    Thread.current[:pod_watch_retry_count] = 0
+    Thread.current[:namespace_watch_retry_count] = 0
+
+    @client = OpenStruct.new
+    def @client.watch_pods(options = {})
+      []
+    end
+    def @client.watch_namespaces(options = {})
+      []
+    end
+    def @client.get_namespaces(options = {})
+      {items: [], metadata: {resourceVersion: '12345'}}
+    end
+    def @client.get_pods(options = {})
+      {items: [], metadata: {resourceVersion: '12345'}}
+    end
+
+    @exception_raised = :blow_up_when_used
+  end
+
+  def watcher=(value)
+  end
+
+  def log
+    logger = {}
+    def logger.debug(message)
+    end
+    def logger.info(message, error)
+    end
+    def logger.error(message, error)
+    end
+    logger
+  end
+end
