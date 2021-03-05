ecc565d0d668c27128446779681c501f946845c6
Patryk Małek <pmalek@sumologic.com>
2021-03-05 12:52:42 +0100

    Refine requirements for sumologic-terraform-provider to ~> 2.8.0

--- deploy/helm/sumologic/conf/setup/main.tf
+++ deploy/helm/sumologic/conf/setup/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     sumologic = {
       source  = "sumologic/sumologic"
-      version = "~> 2.6"
+      version = "~> 2.8.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
