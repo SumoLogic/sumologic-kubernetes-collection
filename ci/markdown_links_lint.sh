763cf838c7f71e25a8b4624445fd9dac05a2583d
Dominik Rosiek <drosiek@sumologic.com>
2021-03-09 10:04:02 +0100

    ci: add markdown links linter
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    Co-authored-by: Patryk Małek <69143962+pmalek-sumo@users.noreply.github.com>

---
+++ ci/markdown_links_lint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Get all markdown files
+readonly FILES=$(find . -type f -name '*.md')
+
+RET_VAL=0
+
+for file in ${FILES}; do
+    # '\[[^\]]*\]\([^\)]*\)' - get all markdown links [*](*)
+    # filter in only linked to this repository
+    # filter out all links pointing to specific release, tag or commit
+    # filter out links ended with /releases
+    grep -HnoP '\[[^\]]*\]\([^\)]*\)' "${file}" \
+        | grep 'sumologic-kubernetes-collection' \
+        | grep -vP '(\/(blob|tree)\/(v\d+\.|[a-f0-9]{40}\/|release\-))' \
+        | grep -vP '\/releases\)'
+    
+    # Set RET_VAL to 1 if grep was successful (found something)
+    if $?; then
+        RET_VAL=1
+    fi
+done
+
+exit "${RET_VAL}"
