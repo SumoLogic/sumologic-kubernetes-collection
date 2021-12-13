d557fdb29705900f6151058731c4932d752ed9dd
Mikolaj Swiatek <mswiatek@sumologic.com>
2021-12-13 18:00:00 +0100

    chore(tests): add a comment explaining the weird string tests

--- tests/integration/internal/strings/strings_test.go
+++ tests/integration/internal/strings/strings_test.go
@@ -6,6 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// Even though the two tests below look identical, they actually take the *test name* itself
+// as the input - the fact that function returns the same result is the point. They are
+// *not* duplicates.
 func Test_ValuesFileFromT(t *testing.T) {
 	assert.Equal(t, "values_valuesfilefromt.yaml", ValueFileFromT(t))
 }
