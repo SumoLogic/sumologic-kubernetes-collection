023c4598a3929ffe147c809729958b6aab88040c
Dominik Rosiek <drosiek@sumologic.com>
2022-04-26 15:44:31 +0200

    chore(shellcheck): validate generated setup shell files
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- ci/shellcheck.sh
+++ ci/shellcheck.sh
@@ -9,3 +9,17 @@ find . ! -path '*deploy/helm/sumologic/conf/setup/setup.sh' ! -path "*/tmp/*" -n
         echo "Checking ${file} with shellcheck"
         shellcheck --enable all --external-sources --exclude SC2155 "${file}"
     done
+
+find . -path '*tests/helm/terraform/static/*.output.yaml' -type 'f' -print |
+    while read -r file; do
+        # Run tests in their own context
+        echo "Checking ${file} with shellcheck"
+        yq r "${file}" "data[setup.sh]" | shellcheck --enable all --external-sources --exclude SC2155 -
+    done
+
+find . -path '*tests/helm/terraform_custom/static/*.output.yaml' ! -path "./tests/helm/terraform_custom/static/empty.output.yaml" -type 'f' -print |
+    while read -r file; do
+        # Run tests in their own context
+        echo "Checking ${file} with shellcheck"
+        yq r "${file}" "data[custom_setup.sh]" | shellcheck --enable all --external-sources --exclude SC2155 -
+    done
