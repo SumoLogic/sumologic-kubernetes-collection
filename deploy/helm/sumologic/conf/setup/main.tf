deaa404956aabafc375ee8603ef675208862bdf9
Dominik Rosiek <drosiek@sumologic.com>
2022-06-30 17:48:01 +0200

    chore: upgrade sumologic terraform provider to 2.16
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- deploy/helm/sumologic/conf/setup/main.tf
+++ deploy/helm/sumologic/conf/setup/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     sumologic = {
       source  = "sumologic/sumologic"
-      version = "~> 2.11"
+      version = "~> 2.16"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
