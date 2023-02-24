3b6f91c5c8d83e393d5e08d9d570f5833355428f
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2023-02-24 17:10:49 +0100

    fix(vagrant): fix provision.sh and completion script (#2895)
    * fix(vagrant): fix provision.sh script
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * fix(vagrant): fix bash behavior
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    ---------
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- vagrant/scripts/sumo-make-completion.sh
+++ vagrant/scripts/sumo-make-completion.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 
 targets=$(grep -oE '^\S*\:' /sumologic/vagrant/Makefile | sed 's/\:$//g')
 complete -W "${targets}" sumo-make
+
+# Restore default behavior for bash
+set +euo pipefail
