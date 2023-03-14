3a613a2f0d2eab11104a24670b96f465e112b0eb
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2023-03-14 15:02:43 +0100

    chore(vagrant): install prettier in vagrant (#2914)
    * chore(vagrant): install prettier in vagrant
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore(vagrant): freeze prettier version
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * Update vagrant/provision.sh
    
    * Update vagrant/provision.sh
    
    ---------
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,6 +3,13 @@
 set -euo pipefail
 
 YQ_VERSION=4.30.8
+PRETTIER_VERSION=2.8.3
+HELM_3_VERSION=v3.8.2
+SHELLCHECK_VERSION=v0.9.0
+K9S_VERSION=v0.24.15
+GO_VERSION=1.19
+KIND_VERSION=v0.17.0
+LSB_RELEASE="$(lsb_release -cs)"
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
@@ -46,8 +53,6 @@ echo "sudo iptables -P FORWARD ACCEPT" >> /home/vagrant/.bashrc
 
 echo "export KUBECONFIG=/var/snap/microk8s/current/credentials/kubelet.config" >> /home/vagrant/.bashrc
 
-HELM_3_VERSION=v3.8.2
-
 mkdir /opt/helm3
 curl "https://get.helm.sh/helm-${HELM_3_VERSION}-linux-amd64.tar.gz" | tar -xz -C /opt/helm3
 
@@ -62,7 +67,6 @@ chmod +x /usr/local/bin/yq-"${YQ_VERSION}"
 ln -s /usr/local/bin/yq-"${YQ_VERSION}" /usr/local/bin/yq
 
 # Install docker
-LSB_RELEASE="$(lsb_release -cs)"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -83,16 +87,15 @@ snap install ruby --channel=2.6/stable --classic
 gem install bundler
 apt install -y gcc g++ libsnappy-dev libicu-dev zlib1g-dev cmake pkg-config libssl-dev
 
-SHELLCHECK_VERSION=v0.7.1
 curl -Lo- "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar -xJf -
 sudo cp "shellcheck-${SHELLCHECK_VERSION}/shellcheck" /usr/local/bin
 rm -rf "shellcheck-${SHELLCHECK_VERSION}/"
 
 npm install -g markdownlint-cli
+npm install -g "prettier@${PRETTIER_VERSION}"
 # shellcheck disable=SC2016
 echo 'export PATH="$PATH:$HOME/.gem/bin"' >> /home/vagrant/.bashrc
 
-K9S_VERSION=v0.24.15
 mkdir /opt/k9s
 curl -Lo- "https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_x86_64.tar.gz" | tar -C /opt/k9s -xzf -
 ln -s /opt/k9s/k9s /usr/bin/k9s
@@ -101,7 +104,6 @@ ln -s /sumologic/vagrant/scripts/sumo-make.sh /usr/local/bin/sumo-make
 ln -s /sumologic/vagrant/scripts/sumo-make-completion.sh /etc/bash_completion.d/sumo-make
 
 # Install Go
-GO_VERSION=1.18
 curl -LJ "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz" -o go.linux-amd64.tar.gz \
     && rm -rf /usr/local/go \
     && tar -C /usr/local -xzf go.linux-amd64.tar.gz \
@@ -109,7 +111,6 @@ curl -LJ "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz" -o go.linux-
     && ln -s /usr/local/go/bin/go /usr/local/bin
 
 # Install Kind
-KIND_VERSION=v0.17.0
 curl -Lo ./kind "https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-linux-amd64"
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
