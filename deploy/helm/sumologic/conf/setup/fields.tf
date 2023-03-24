933619b094d8e0069bdf3784fca8f2ff34492100
Dominik Rosiek <drosiek@sumologic.com>
2023-03-24 16:41:46 +0100

    chore: add comment to terraform ignore_changes
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- deploy/helm/sumologic/conf/setup/fields.tf
+++ deploy/helm/sumologic/conf/setup/fields.tf
@@ -3,6 +3,8 @@ resource "sumologic_field" {{ $value | quote }} {
   count = var.create_fields ? 1 : 0
 
   lifecycle {
+    # ignore changes for name and type, as older installations have been case sensitive
+    # see: https://github.com/SumoLogic/sumologic-kubernetes-collection/issues/2865
     ignore_changes = [field_name, data_type]
   }
 
