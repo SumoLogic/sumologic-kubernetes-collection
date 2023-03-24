8139f94ac67740ff819a269814f91e7e2229ed51
Dominik Rosiek <drosiek@sumologic.com>
2023-03-24 13:01:46 +0100

    chore: ignore name changes for fields
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- deploy/helm/sumologic/conf/setup/fields.tf
+++ deploy/helm/sumologic/conf/setup/fields.tf
@@ -2,6 +2,10 @@
 resource "sumologic_field" {{ $value | quote }} {
   count = var.create_fields ? 1 : 0
 
+  lifecycle {
+    ignore_changes = ["field_name", "data_type"]
+  }
+
   field_name = {{ $value | quote }}
   data_type = "String"
   state = "Enabled"
