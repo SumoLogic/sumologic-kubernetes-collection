be6a931703732daf7c8091864fe42e0161f73775
Dominik Rosiek <drosiek@sumologic.com>
2023-03-24 14:50:03 +0100

    chore: improve terraform
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- deploy/helm/sumologic/conf/setup/fields.tf
+++ deploy/helm/sumologic/conf/setup/fields.tf
@@ -3,7 +3,7 @@ resource "sumologic_field" {{ $value | quote }} {
   count = var.create_fields ? 1 : 0
 
   lifecycle {
-    ignore_changes = ["field_name", "data_type"]
+    ignore_changes = [field_name, data_type]
   }
 
   field_name = {{ $value | quote }}
