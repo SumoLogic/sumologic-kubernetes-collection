eee8fe6c16b270d703d05901277ee52aeb34c0ec
Mikołaj Świątek <mswiatek@sumologic.com>
2023-05-19 13:55:35 +0200

    test(integration): check for recording rule metrics

--- tests/integration/internal/constants.go
+++ tests/integration/internal/constants.go
@@ -179,6 +179,34 @@ var (
 		"otelcol_loadbalancer_num_backends",
 		"otelcol_loadbalancer_num_resolutions",
 	}
+	RecordingRuleMetrics = []string{
+		":kube_pod_info_node_count:",
+		"node:node_cpu_utilisation:avg1m",
+		":node_memory_utilisation:",
+		// "node:node_memory_bytes_available:sum", // not present, depends on other recording rules that don't exist
+		"node:node_memory_utilisation:ratio",
+		"node:node_memory_utilisation:",
+		"node:node_memory_utilisation_2:",
+		"node:node_filesystem_usage:",
+		"node:node_memory_bytes_total:sum",
+		":node_net_utilisation:sum_irate",
+		"node:node_net_utilisation:sum_irate",
+		":node_net_saturation:sum_irate",
+		"node:node_net_saturation:sum_irate",
+		":node_cpu_saturation_load1:",
+		":node_disk_saturation:avg_irate",
+		"node:node_disk_saturation:avg_irate",
+		":node_disk_utilisation:avg_irate",
+		"node:node_disk_utilisation:avg_irate",
+		":node_memory_swap_io_bytes:sum_rate",
+		"node:node_memory_swap_io_bytes:sum_rate",
+		"node:cluster_cpu_utilisation:ratio",
+		"node:cluster_memory_utilisation:ratio",
+		"node:node_cpu_saturation_load1:",
+		"node:node_filesystem_avail:",
+		// "node:node_inodes_total:", // looks like we're not collecting node_filesystem_files which this requires
+		// "node:node_inodes_free:",  // looks like we're not collecting node_filesystem_files_free which this requires
+	}
 )
 
 var (
@@ -198,6 +226,7 @@ var (
 		CoreDNSMetrics,
 		CAdvisorMetrics,
 		NodeExporterMetrics,
+		RecordingRuleMetrics,
 	}
 	DefaultExpectedMetrics                 []string
 	DefaultExpectedFluentdFluentbitMetrics []string
