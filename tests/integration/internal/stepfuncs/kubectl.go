ffc2757e7a532cdcc7918b56b86d74e5a6eb22c1
Rupesh Nemade <rupesh.nemade@acquia.com>
2023-06-09 17:53:40 +0200

    fixed lint issues

--- tests/integration/internal/stepfuncs/kubectl.go
+++ tests/integration/internal/stepfuncs/kubectl.go
@@ -8,7 +8,6 @@ import (
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 
-	"github.com/SumoLogic/sumologic-kubernetes-collection/tests/integration/internal"
 	"github.com/SumoLogic/sumologic-kubernetes-collection/tests/integration/internal/ctxopts"
 	"github.com/SumoLogic/sumologic-kubernetes-collection/tests/integration/internal/strings"
 )
@@ -53,8 +52,7 @@ func KubectlCreateNamespaceTestOpt() features.Func {
 
 func KubectlOverrideNamespaceOpt(namespace string) features.Func {
 	return func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
-		kubectlOptions.Namespace = namespace
-		return KubectlCreateNamespaceOpt(name)(ctx, t, envConf)
+		return KubectlCreateNamespaceOpt(namespace)(ctx, t, envConf)
 	}
 }
 
