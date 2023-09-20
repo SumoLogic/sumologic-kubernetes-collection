a8f22f7e07370d4700dc039db5b69654c2798042
Andrzej Stencel <astencel@sumologic.com>
2023-09-20 11:59:38 +0200

    feat(logs): add `sumologic.logs.additionalFields` property (#3286)

--- deploy/helm/sumologic/conf/setup/fields.tf
+++ deploy/helm/sumologic/conf/setup/fields.tf
@@ -13,3 +13,19 @@ resource "sumologic_field" {{ $value | quote }} {
   state = "Enabled"
 }
 {{- end }}
+
+{{- range $value := .Values.sumologic.logs.additionalFields }}
+resource "sumologic_field" {{ $value | quote }} {
+  count = var.create_fields ? 1 : 0
+
+  lifecycle {
+    # ignore changes for name and type, as older installations have been case sensitive
+    # see: https://github.com/SumoLogic/sumologic-kubernetes-collection/issues/2865
+    ignore_changes = [field_name, data_type]
+  }
+
+  field_name = {{ $value | quote }}
+  data_type = "String"
+  state = "Enabled"
+}
+{{- end }}
