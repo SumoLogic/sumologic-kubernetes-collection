f13ad108a4bb216e207e8f8b5da5b7cabe0d7ed5
Mikołaj Świątek <mail+sumo@mikolajswiatek.com>
2023-10-19 18:46:32 +0200

    chore: use nix for installing tools in Vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -1,15 +1,9 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-unless Vagrant.has_plugin?("vagrant-disksize")
-  puts "vagrant-disksize plugin unavailable\n" +
-       "please install it via 'vagrant plugin install vagrant-disksize'"
-  exit 1
-end
-
 Vagrant.configure('2') do |config|
   config.vm.box = 'ubuntu/focal64'
-  config.disksize.size = '50GB'
+  config.vm.disk :disk, size: "50GB", primary: true
   config.vm.box_check_update = false
   config.vm.host_name = 'sumologic-kubernetes-collection'
   # Vbox 6.1.28+ restricts host-only adapters to 192.168.56.0/21
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -2,19 +2,13 @@
 
 set -euo pipefail
 
-YQ_VERSION=4.33.2
-PRETTIER_VERSION=2.8.4
-HELM_3_VERSION=v3.11.2
-SHELLCHECK_VERSION=v0.9.0
-K9S_VERSION=v0.24.15
-GO_VERSION=1.20
-KIND_VERSION=v0.17.0
 LSB_RELEASE="$(lsb_release -cs)"
+ARCH="$(dpkg --print-architecture)"
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get --yes upgrade
-apt-get --yes install apt-transport-https jq make npm yamllint
+apt-get --yes install apt-transport-https make ca-certificates curl gnupg
 
 echo "export EDITOR=vim" >> /home/vagrant/.bashrc
 
@@ -53,27 +47,32 @@ echo "sudo iptables -P FORWARD ACCEPT" >> /home/vagrant/.bashrc
 
 echo "export KUBECONFIG=/var/snap/microk8s/current/credentials/kubelet.config" >> /home/vagrant/.bashrc
 
-mkdir /opt/helm3
-curl "https://get.helm.sh/helm-${HELM_3_VERSION}-linux-amd64.tar.gz" | tar -xz -C /opt/helm3
-
-ln -s /opt/helm3/linux-amd64/helm /usr/bin/helm3
-ln -s /usr/bin/helm3 /usr/bin/helm
-
 usermod -a -G microk8s vagrant
-
-# install yq with access to file structure
-curl "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64" -L -o /usr/local/bin/yq-"${YQ_VERSION}"
-chmod +x /usr/local/bin/yq-"${YQ_VERSION}"
-ln -s /usr/local/bin/yq-"${YQ_VERSION}" /usr/local/bin/yq
 
 # Install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=${ARCH}] https://download.docker.com/linux/ubuntu \
    ${LSB_RELEASE} \
    stable"
 apt-get install -y docker-ce docker-ce-cli containerd.io
 usermod -aG docker vagrant
+
+# Install nix
+curl -L https://nixos.org/nix/install -o /tmp/install-nix.sh
+command -v nix-shell || sh /tmp/install-nix.sh --daemon --yes
+# shellcheck source=/dev/null
+source "/etc/bashrc"
+
+# install k9s and direnv
+nix-env -i k9s direnv
+
+# set up direnv and the nix shell environment
+echo "eval \"\$(direnv hook bash)\"" >> /home/vagrant/.bashrc
+echo "use nix /sumologic/shell.nix" > /home/vagrant/.envrc
+
+echo "Building Nix shell environment"
+sudo -u vagrant bash -c "source /etc/bashrc; direnv allow /home/vagrant; cd /home/vagrant; nix-shell /sumologic/shell.nix"
 
 # K8s needs some time to bootstrap
 while true; do
@@ -82,42 +81,8 @@ while true; do
   sleep 1
 done
 
-# install requirements for ci/build.sh
-snap install ruby --channel=2.6/stable --classic
-gem install bundler
-apt install -y gcc g++ libsnappy-dev libicu-dev zlib1g-dev cmake pkg-config libssl-dev
-
-curl -Lo- "https://github.com/koalaman/shellcheck/releases/download/${SHELLCHECK_VERSION}/shellcheck-${SHELLCHECK_VERSION}.linux.x86_64.tar.xz" | tar -xJf -
-sudo cp "shellcheck-${SHELLCHECK_VERSION}/shellcheck" /usr/local/bin
-rm -rf "shellcheck-${SHELLCHECK_VERSION}/"
-
-npm install -g markdownlint-cli
-npm install -g "prettier@${PRETTIER_VERSION}"
-# shellcheck disable=SC2016
-echo 'export PATH="$PATH:$HOME/.gem/bin"' >> /home/vagrant/.bashrc
-
-mkdir /opt/k9s
-curl -Lo- "https://github.com/derailed/k9s/releases/download/${K9S_VERSION}/k9s_Linux_x86_64.tar.gz" | tar -C /opt/k9s -xzf -
-ln -s /opt/k9s/k9s /usr/bin/k9s
-
-ln -s /sumologic/vagrant/scripts/sumo-make.sh /usr/local/bin/sumo-make
-ln -s /sumologic/vagrant/scripts/sumo-make-completion.sh /etc/bash_completion.d/sumo-make
-
-# Install Go
-curl -LJ "https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz" -o go.linux-amd64.tar.gz \
-    && rm -rf /usr/local/go \
-    && tar -C /usr/local -xzf go.linux-amd64.tar.gz \
-    && rm go.linux-amd64.tar.gz \
-    && ln -s /usr/local/go/bin/go /usr/local/bin
-
-# Install Kind
-curl -Lo ./kind "https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-linux-amd64"
-chmod +x ./kind
-mv ./kind /usr/local/bin/kind
-
-# install python with dependencies
-apt install -y python3 python3-pip
-pip install pyyaml
+ln -sf /sumologic/vagrant/scripts/sumo-make.sh /usr/local/bin/sumo-make
+ln -sf /sumologic/vagrant/scripts/sumo-make-completion.sh /etc/bash_completion.d/sumo-make
 
 # print out summary
 echo Dashboard local in-vagrant IP:
@@ -126,4 +91,4 @@ echo
 
 echo Dashboard token:
 /sumologic/vagrant/scripts/get-dashboard-token.sh
-echo
+
