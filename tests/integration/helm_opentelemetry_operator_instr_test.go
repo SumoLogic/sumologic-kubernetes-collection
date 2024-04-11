0ebf19a919c05753c88ff0c2768b06581e1d83fb
Mateusz Rumian <mrumian@sumologic.com>
2024-04-11 14:43:52 +0200

    chore: add instrumentation tests

---
+++ tests/integration/helm_opentelemetry_operator_instr_test.go
@@ -0,0 +1,144 @@
+//go:build allversions
+// +build allversions
+
+package integration
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	otoperatorappsv1 "github.com/open-telemetry/opentelemetry-operator/apis/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/e2e-framework/klient/k8s/resources"
+	"sigs.k8s.io/e2e-framework/klient/wait"
+	"sigs.k8s.io/e2e-framework/klient/wait/conditions"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/SumoLogic/sumologic-kubernetes-collection/tests/integration/internal/ctxopts"
+)
+
+type internalCtxKey string
+
+func Test_OpenTelemetry_Operator_Instrumentation(t *testing.T) {
+
+	const (
+		tickDuration = time.Second
+		waitDuration = time.Minute * 2
+	)
+
+	var originalNamespaceKey internalCtxKey = "originalNamespace"
+
+	installChecks := []featureCheck{
+		CheckSumologicSecret(3),
+		CheckTracesWithoutGatewayInstall,
+	}
+
+	featInstall := GetInstallFeature(installChecks)
+
+	// It is required to add v1alpha1 OT Operator Scheme to K8s Scheme
+	// https://github.com/open-telemetry/opentelemetry-operator/issues/772
+	if err := otoperatorappsv1.AddToScheme(scheme.Scheme); err != nil {
+		require.Fail(t, "failed to register scheme: %v", err)
+	}
+
+	featOpenTelemetryOperator := features.New("opentelemetry-operator").
+		// TODO: Rewrite into similar step func as WaitUntilStatefulSetIsReady but for deployments
+		Assess("opentelemetry-operator deployment is ready", func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+			res := envConf.Client().Resources(ctxopts.Namespace(ctx))
+			labelSelector := "app.kubernetes.io/name=opentelemetry-operator"
+			ds := appsv1.DeploymentList{}
+
+			require.NoError(t,
+				wait.For(
+					conditions.New(res).
+						ResourceListN(&ds, 1,
+							resources.WithLabelSelector(labelSelector),
+						),
+					wait.WithTimeout(waitDuration),
+					wait.WithInterval(tickDuration),
+				),
+			)
+			require.NoError(t,
+				wait.For(
+					conditions.New(res).
+						DeploymentConditionMatch(&ds.Items[0], appsv1.DeploymentAvailable, corev1.ConditionTrue),
+					wait.WithTimeout(waitDuration),
+					wait.WithInterval(tickDuration),
+				),
+			)
+			return ctx
+		}).
+		Assess("instrumentation-cr in ot-operator1 namespace is created", func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+			res := envConf.Client().Resources("ot-operator1")
+			releaseName := ctxopts.HelmRelease(ctx)
+			labelSelector := fmt.Sprintf("app=%s-sumologic-ot-operator-instr", releaseName)
+			instrs := otoperatorappsv1.InstrumentationList{}
+
+			require.NoError(t,
+				wait.For(
+					conditions.New(res).
+						ResourceListN(&instrs, 1,
+							resources.WithLabelSelector(labelSelector),
+						),
+					wait.WithTimeout(waitDuration),
+					wait.WithInterval(tickDuration),
+				),
+			)
+			return ctx
+		}).
+		Assess("instrumentation-cr in ot-operator2 namespace is created", func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
+			res := envConf.Client().Resources("ot-operator2")
+			releaseName := ctxopts.HelmRelease(ctx)
+			labelSelector := fmt.Sprintf("app=%s-sumologic-ot-operator-instr", releaseName)
+			instrs := otoperatorappsv1.InstrumentationList{}
+
+			require.NoError(t,
+				wait.For(
+					conditions.New(res).
+						ResourceListN(&instrs, 1,
+							resources.WithLabelSelector(labelSelector),
+						),
+					wait.WithTimeout(waitDuration),
+					wait.WithInterval(tickDuration),
+				),
+			)
+
+			return ctx
+		}).
+		Feature()
+
+	featDotnetApp := GetDotnetAppFeature()
+	featJavaApp := GetJavaAppFeature()
+	featNodeJSApp := GetNodeJSAppFeature()
+	featPythonApp := GetPythonAppFeature()
+
+	curlApp := GetCurlAppFeature()
+
+	useDefaultNs := func(ctx context.Context, envConf *envconf.Config, t *testing.T, _ features.Feature) (context.Context, error) {
+		originalNamespace := ctxopts.Namespace(ctx)
+		ctx = context.WithValue(ctx, originalNamespaceKey, originalNamespace)
+		kubectlOptions := ctxopts.KubectlOptions(ctx)
+		kubectlOptions.Namespace = originalNamespace
+		ctx = ctxopts.WithKubectlOptions(ctx, kubectlOptions)
+		ctx = ctxopts.WithNamespace(ctx, originalNamespace)
+		return ctx, nil
+	}
+
+	restoreOriginalNamespace := func(ctx context.Context, envConf *envconf.Config, t *testing.T, _ features.Feature) (context.Context, error) {
+		originalNamespace := ctx.Value(originalNamespaceKey).(string)
+		kubectlOptions := ctxopts.KubectlOptions(ctx)
+		kubectlOptions.Namespace = originalNamespace
+		ctx = ctxopts.WithKubectlOptions(ctx, kubectlOptions)
+		ctx = ctxopts.WithNamespace(ctx, originalNamespace)
+		return ctx, nil
+	}
+
+	testenv.BeforeEachFeature(useDefaultNs).AfterEachFeature(restoreOriginalNamespace).Test(t, featInstall, featOpenTelemetryOperator, featDotnetApp, featJavaApp, featNodeJSApp, featPythonApp, curlApp)
+}
