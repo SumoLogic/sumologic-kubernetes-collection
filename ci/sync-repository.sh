b29acbf7556afe5b42790078c77ea2ca9c54c064
Dominik Rosiek <drosiek@sumologic.com>
2024-04-23 17:00:08 +0200

    chore: test running in docker
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- ci/sync-repository.sh
+++ ci/sync-repository.sh
@@ -2,10 +2,15 @@
 
 SRC_REPOSITORY=${1}
 DESTINATION_NAMESPACE=${2}
-skopeo sync \
-    -f v2s2 \
-    --retry-times 5 \
-    --src docker \
-    --dest docker \
-    "${SRC_REPOSITORY}" \
-    "${DESTINATION_NAMESPACE}"
+ls -al ~/.docker/config.json
+docker run \
+    -v ~/.docker/config.json:/tmp/auth.json \
+    quay.io/skopeo/stable:v1.15.0 \
+        sync \
+            --keep-going \
+            -f v2s2 \
+            --retry-times 5 \
+            --src docker \
+            --dest docker \
+            "${SRC_REPOSITORY}" \
+            "${DESTINATION_NAMESPACE}"
