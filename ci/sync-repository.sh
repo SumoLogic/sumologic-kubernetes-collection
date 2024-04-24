03efe655a4536d1314a10ed9a95507c5ed98e20e
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2024-04-24 21:28:01 +0200

    chore: add remaining repositories for syncing  (#3676)
    * chore: add remaining repositories for syncing
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: remove re-run lock
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: use oci format
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: run on ubuntu 22.04
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: run on ubuntu 24.04
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: use ubuntu 22.04
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: test running in docker
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: cleanup
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: skip signatures
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: try oci format
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: do not abort other jobs if one fails
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: try to sync ECR images event if Docker Hub sync fails
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * feat: add kube-prometheus-stack and nginx images
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: do not sync images if login fails
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: no need to install skopeo
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: add prometheus repository
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: repository name for prometheus
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: fix repository for prometheus-operator
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: sort repositories alphabetically
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    ---------
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- ci/sync-repository.sh
+++ ci/sync-repository.sh
@@ -2,10 +2,14 @@
 
 SRC_REPOSITORY=${1}
 DESTINATION_NAMESPACE=${2}
-skopeo sync \
-    -f v2s2 \
-    --retry-times 5 \
-    --src docker \
-    --dest docker \
-    "${SRC_REPOSITORY}" \
-    "${DESTINATION_NAMESPACE}"
+docker run \
+    -v ~/.docker/config.json:/tmp/auth.json \
+    quay.io/skopeo/stable:v1.15.0 \
+        sync \
+            --keep-going \
+            -f oci \
+            --retry-times 5 \
+            --src docker \
+            --dest docker \
+            "${SRC_REPOSITORY}" \
+            "${DESTINATION_NAMESPACE}"
