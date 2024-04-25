51afbe295ff7759ecd842d559517ebb6103dedda
Dominik Rosiek <drosiek@sumologic.com>
2024-04-25 14:33:39 +0200

    .
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- ci/sync-repository.sh
+++ ci/sync-repository.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-
+set -x
 SRC_REPOSITORY=${1}
 DESTINATION_NAMESPACE=${2}
+FORMAT="${3}"
+
+if [[ -n "${FORMAT}" ]]; then
+    FORMAT="-f ${FORMAT}"
+fi
+
 docker run \
     -v ~/.docker/config.json:/tmp/auth.json \
     quay.io/skopeo/stable:v1.15.0 \
         sync \
+            ${FORMAT} \
             --keep-going \
             --retry-times 5 \
             --src docker \
