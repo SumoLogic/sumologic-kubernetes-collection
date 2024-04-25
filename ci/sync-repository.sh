f384bb4172da52ec208bad9bd0cc26e372e123b4
Dominik Rosiek <drosiek@sumologic.com>
2024-04-25 17:35:31 +0200

    chore: copy all manifest and do not force format
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- ci/sync-repository.sh
+++ ci/sync-repository.sh
@@ -2,17 +2,11 @@
 set -x
 SRC_REPOSITORY=${1}
 DESTINATION_NAMESPACE=${2}
-FORMAT="${3}"
-
-if [[ -n "${FORMAT}" ]]; then
-    FORMAT="-f ${FORMAT}"
-fi
 
 docker run \
     -v ~/.docker/config.json:/tmp/auth.json \
     quay.io/skopeo/stable:v1.15.0 \
         sync \
-            ${FORMAT} \
             --all \
             --keep-going \
             --retry-times 5 \
