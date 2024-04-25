ddfb8d35c8a1c3a5f9ec05ba1c4d8a537f617476
Dominik Rosiek <58699848+sumo-drosiek@users.noreply.github.com>
2024-04-25 18:04:05 +0200

    chore: fix syncing manifest (#3678)
    * chore: use default format
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * .
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: use all flag
    
    * chore: copy all manifest and do not force format
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    * chore: restore oci format
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>
    
    ---------
    
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- ci/sync-repository.sh
+++ ci/sync-repository.sh
@@ -2,10 +2,12 @@
 
 SRC_REPOSITORY=${1}
 DESTINATION_NAMESPACE=${2}
+
 docker run \
     -v ~/.docker/config.json:/tmp/auth.json \
     quay.io/skopeo/stable:v1.15.0 \
         sync \
+            --all \
             --keep-going \
             -f oci \
             --retry-times 5 \
