739896b77d599ea649a00c063918987afed206da
Mikołaj Świątek <mail+sumo@mikolajswiatek.com>
2024-06-03 13:56:45 +0200

    test(integration): don't store helm options in context
    Instead of storing helm options in the context, we can simply
    recalculate them from existing context properties. This makes the test
    setup more explicit.

--- tests/integration/internal/ctxopts/context_opts.go
+++ tests/integration/internal/ctxopts/context_opts.go
@@ -3,7 +3,6 @@ package ctxopts
 import (
 	"context"
 
-	"github.com/gruntwork-io/terratest/modules/helm"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 )
 
@@ -24,15 +23,6 @@ func WithKubectlOptions(ctx context.Context, kubectlOptions *k8s.KubectlOptions)
 func KubectlOptions(ctx context.Context) *k8s.KubectlOptions {
 	v := ctx.Value(ctxKeyNameKubectlOptions)
 	return v.(*k8s.KubectlOptions)
-}
-
-func WithHelmOptions(ctx context.Context, helmOptions *helm.Options) context.Context {
-	return context.WithValue(ctx, ctxKeyNameHelmOptions, helmOptions)
-}
-
-func HelmOptions(ctx context.Context) *helm.Options {
-	v := ctx.Value(ctxKeyNameHelmOptions)
-	return v.(*helm.Options)
 }
 
 func WithNamespace(ctx context.Context, namespace string) context.Context {
