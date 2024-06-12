69b082643d4cb75606699ec217fe147172f23251
Mikołaj Świątek <mail+sumo@mikolajswiatek.com>
2024-06-12 16:18:07 +0200

    chore(setup): store source configs as json

--- deploy/helm/sumologic/conf/setup/resources.tf
+++ deploy/helm/sumologic/conf/setup/resources.tf
@@ -16,23 +16,7 @@ resource "kubernetes_secret" "sumologic_collection_secret" {
   }
 
   data = {
-    {{- $ctx := .Values }}
-    {{- range $type, $sources := .Values.sumologic.collector.sources }}
-      {{- if eq (include "terraform.sources.component_enabled" (dict "Values" $ctx "Type" $type)) "true" }}
-        {{- range $key, $source := $sources }}
-          {{- if eq (include "terraform.sources.to_create" (dict "Context" $ctx "Type" $type "Name" $key)) "true" }}
-    {{ include "terraform.sources.data" (dict "Endpoint" (include "terraform.sources.config-map-variable" (dict "Type" $type "Context" $ctx "Name" $key)) "Name" (include "terraform.sources.name" (dict "Name" $key "Type" $type))) }}
-          {{- end }}
-        {{- end }}
-      {{- else if and (eq $type "metrics") $ctx.sumologic.traces.enabled }}
-        {{- /*
-        If traces are enabled and metrics are disabled, create default metrics source anyway
-        */}}
-        {{- if hasKey $sources "default" }}
-    {{ include "terraform.sources.data" (dict "Endpoint" (include "terraform.sources.config-map-variable" (dict "Type" $type "Context" $ctx "Name" "default")) "Name" (include "terraform.sources.name" (dict "Name" "default" "Type" $type))) }}
-        {{- end }}
-      {{- end }}
-    {{- end }}
+    for name, config in local.source_configs : config["config-name"] => lookup(local.sources, name).url
   }
 
   type = "Opaque"
