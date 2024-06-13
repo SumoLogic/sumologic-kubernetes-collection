79690d52e1fd2ed264c3cf0160e0e31a0f428ddd
Mateusz "mat" Rumian <58699800+mat-rumian@users.noreply.github.com>
2024-06-13 14:34:11 +0200

    chore(instrumentation): move instrumentation configuration from opentelemetry-operator (#3733)
    * chore: instr update templates
    * chore: instr update values
    * chore: update instr tests
    * chore: update integration instr tests
    * chore: nodejs add metrics exporter flag
    * chore: update parameters list
    * chore: add changelog

--- tests/integration/internal/stepfuncs/kubectl.go
+++ tests/integration/internal/stepfuncs/kubectl.go
@@ -61,15 +61,15 @@ func KubectlCreateOperatorNamespacesOpt() features.Func {
 	return func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
 		valuesFileBytes := GetHelmValuesForT(t)
 		var values struct {
-			Operator struct {
+			Instrumentation struct {
 				InstrumentationNamespaces string `yaml:"instrumentationNamespaces"`
-			} `yaml:"opentelemetry-operator"`
+			} `yaml:"instrumentation"`
 		}
 
 		err := yaml.Unmarshal(valuesFileBytes, &values)
 		require.NoError(t, err)
-		if values.Operator.InstrumentationNamespaces != "" {
-			namespaces := stdstrings.Split(values.Operator.InstrumentationNamespaces, ",")
+		if values.Instrumentation.InstrumentationNamespaces != "" {
+			namespaces := stdstrings.Split(values.Instrumentation.InstrumentationNamespaces, ",")
 			for _, namespace := range namespaces {
 				k8s.CreateNamespace(t, ctxopts.KubectlOptions(ctx, envConf), namespace)
 			}
@@ -83,15 +83,15 @@ func KubectlDeleteOperatorNamespacesOpt() features.Func {
 	return func(ctx context.Context, t *testing.T, envConf *envconf.Config) context.Context {
 		valuesFileBytes := GetHelmValuesForT(t)
 		var values struct {
-			Operator struct {
+			Instrumentation struct {
 				InstrumentationNamespaces string `yaml:"instrumentationNamespaces"`
-			} `yaml:"opentelemetry-operator"`
+			} `yaml:"instrumentation"`
 		}
 
 		err := yaml.Unmarshal(valuesFileBytes, &values)
 		require.NoError(t, err)
-		if values.Operator.InstrumentationNamespaces != "" {
-			namespaces := stdstrings.Split(values.Operator.InstrumentationNamespaces, ",")
+		if values.Instrumentation.InstrumentationNamespaces != "" {
+			namespaces := stdstrings.Split(values.Instrumentation.InstrumentationNamespaces, ",")
 			for _, namespace := range namespaces {
 				ctx = KubectlDeleteNamespaceOpt(namespace, true)(ctx, t, envConf)
 			}
