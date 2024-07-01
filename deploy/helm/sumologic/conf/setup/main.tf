37f546a803835d9841e63d5a003449f40242f27d
Dominik Rosiek <drosiek@sumologic.com>
2024-07-01 09:06:21 +0200

    chore: upgrade terraform providers versions in setup
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- deploy/helm/sumologic/conf/setup/main.tf
+++ deploy/helm/sumologic/conf/setup/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     sumologic = {
       source  = "sumologic/sumologic"
-      version = ">= 2.28.3, < 3.0.0"
+      version = ">= 2.31.0, < 3.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.4"
+      version = "~> 2.31.0"
     }
   }
 }
