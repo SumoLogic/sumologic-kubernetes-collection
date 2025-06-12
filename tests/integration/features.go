6fa9e9c3d54ddf92fab75bf6806c446661486dc4
Raj Nishtala <rnishtala@sumologic.com>
2025-06-12 16:47:10 -0400

    chore: Reduce the expected log count in the tests

--- tests/integration/features.go
+++ tests/integration/features.go
@@ -35,7 +35,7 @@ const (
 	waitDuration = 1 * time.Minute
 	// number determined experimentally
 	expectedEventCount uint = 50
-	logsGeneratorCount uint = 1000
+	logsGeneratorCount uint = 600
 	// number of log records in single loop with default multiline support only, see: tests/integration/yamls/pod_multiline_long_lines.yaml
 	logRecords = 4 + 10
 	// number of log records in single loop with multiple multilines support, see: tests/integration/yamls/pod_multiline_long_lines.yaml
