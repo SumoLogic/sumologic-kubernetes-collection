4e04a7bec4b3f8bac661343c3fd0a7a0471500d8
Samiur Arif <samiur.arif@sumologic.com>
2025-07-03 17:15:51 -0700

    bumped terraform on sumologic's provider terraform

--- deploy/helm/sumologic/conf/setup/main.tf
+++ deploy/helm/sumologic/conf/setup/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     sumologic = {
       source  = "sumologic/sumologic"
-      version = ">= 3.0.9, < 3.1.0"
+      version = "3.0.12"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
