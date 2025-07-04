45adc0dfe420e3f9af10e2bc666c5977eba2df96
Samiur Arif <samiur.arif@sumologic.com>
2025-07-03 17:37:10 -0700

    fixed broken tests

--- deploy/helm/sumologic/conf/setup/main.tf
+++ deploy/helm/sumologic/conf/setup/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     sumologic = {
       source  = "sumologic/sumologic"
-      version = "3.0.12"
+      version = ">= 3.0.0, <= 3.1.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
