30734f65e14eabe57a68f5a62f74cfe275144452
Jaganathan P <156800415+jagan2221@users.noreply.github.com>
2025-09-04 00:15:24 +0530

    Update helm_ot_default_ipv6_test.go

--- tests/integration/helm_ot_default_ipv6_test.go
+++ tests/integration/helm_ot_default_ipv6_test.go
@@ -11,32 +11,11 @@ import (
 	"github.com/SumoLogic/sumologic-kubernetes-collection/tests/integration/internal/stepfuncs"
 )
 
-func FilterOutIPv6ExcludedMetrics(metrics []string) []string {
-    ipv6ExcludedMetrics := []string{ //Add metrics that are not relevant for IPv6
-        "coredns_cache_hits_total",
-    }
-    var filtered []string
-    for _, m := range metrics {
-        exclude := false
-        for _, excl := range ipv6ExcludedMetrics {
-            if strings.Contains(m, excl) {
-                exclude = true
-                break
-            }
-        }
-        if !exclude {
-            filtered = append(filtered, m)
-        }
-    }
-    return filtered
-}
-
 func Test_Helm_Default_OT_ipv6(t *testing.T) {
 
 	expectedMetrics := internal.DefaultExpectedMetrics
 	
 	expectedMetrics = append(expectedMetrics, internal.TracingOtelcolMetrics...)
-	expectedMetrics = FilterOutIPv6ExcludedMetrics(expectedMetrics)
 
 	installChecks := []featureCheck{
 		CheckSumologicSecret(15),
