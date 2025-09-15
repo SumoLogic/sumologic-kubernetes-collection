3bf6767119a7f9852165dc50a907eb6bfedd1f05
Samiur Arif <samiur.arif@sumologic.com>
2025-09-15 14:38:46 -0700

    added node labels

--- tests/integration/helm_k8s_processor_test.go
+++ tests/integration/helm_k8s_processor_test.go
@@ -15,6 +15,7 @@ func Test_Helm_K8s_Processor(t *testing.T) {
 	expectedMetrics := internal.DefaultExpectedMetrics
 	// we have tracing enabled, so check tracing-specific metrics
 	expectedMetrics = append(expectedMetrics, internal.TracingOtelcolMetrics...)
+	expectedMetrics = append(expectedMetrics, internal.NodeLabelsMetrics...)
 
 	installChecks := []featureCheck{
 		CheckSumologicSecret(15),
