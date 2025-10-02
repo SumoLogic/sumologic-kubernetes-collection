138daa35aef0c4fcd674d8779c9ae8c5c5d097e5
Samiur Arif <samiur.arif@sumologic.com>
2025-10-02 15:52:34 -0700

    bump terraform provider

--- deploy/helm/sumologic/conf/setup/main.tf
+++ deploy/helm/sumologic/conf/setup/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     sumologic = {
       source  = "sumologic/sumologic"
-      version = ">= 2.31.0, < 3.0.0"
+      version = ">= 3.0.0, < 3.1.5"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.32.0"
+      version = "~> 2.38.0"
     }
   }
 }
