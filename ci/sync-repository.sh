27887266747439fb6833c2f767bee4194a87f447
Ravishankar Gnanaprakasam <r.gnanaprakasam@sumologic.com>
2026-03-09 16:38:41 +0530

    feat(SUMO-275536): Sync OSS metrics server
    feat(SUMO-275536): yaml lint fix for changed files
    
    feat(SUMO-275536): fix metrics server config
    
    update config path in script
    
    fix shell check
    
    feat(SUMO-275536): Update busybox yaml to sync recent images
    
    fix busybox yaml config
    
    yaml lint fix
    
    sync metrics server 0.8.0 and above

--- ci/sync-repository.sh
+++ ci/sync-repository.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
-SRC_REPOSITORY=${1}
+SRC_YAML_PATH=${1}
 DESTINATION_NAMESPACE=${2}
+
+WORKDIR="$(pwd)"
 
 docker run \
     -v ~/.docker/config.json:/tmp/auth.json \
+    -v "${WORKDIR}:/workspace" \
     quay.io/skopeo/stable:v1.15.0 \
         sync \
             --all \
             --keep-going \
             --preserve-digests \
             --retry-times 5 \
-            --src docker \
+            --src yaml \
             --src-no-creds \
             --dest docker \
-            "${SRC_REPOSITORY}" \
+            "/workspace/${SRC_YAML_PATH}" \
             "${DESTINATION_NAMESPACE}"
