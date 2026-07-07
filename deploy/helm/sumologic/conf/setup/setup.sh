5b0fd846d59ef9a36e6b0d4a9e2b4ecbaaca7e2a
Jaganathan P <jaganathan.p@sumologic.com>
2026-07-07 22:22:42 +0530

    ..

--- deploy/helm/sumologic/conf/setup/setup.sh
+++ deploy/helm/sumologic/conf/setup/setup.sh
@@ -173,10 +173,14 @@ function delete_hosted_collector() {
     COLLECTOR_ID=$(head -1 <<< "${JQ_OUTPUT}")
     if [[ -n "${COLLECTOR_ID}" ]]; then
         echo "Deleting hosted collector '${SUMOLOGIC_COLLECTOR_NAME}' (${COLLECTOR_ID}) and all its sources..."
-        curl -sf -XDELETE \
+        if curl -sf -XDELETE \
             -u "${SUMOLOGIC_ACCESSID}:${SUMOLOGIC_ACCESSKEY}" \
-            "${SUMOLOGIC_BASE_URL}v1/collectors/${COLLECTOR_ID}" \
-            && echo "Hosted collector deleted." || { echo "Failed to delete hosted collector '${SUMOLOGIC_COLLECTOR_NAME}' (${COLLECTOR_ID})."; return 1; }
+            "${SUMOLOGIC_BASE_URL}v1/collectors/${COLLECTOR_ID}"; then
+            echo "Hosted collector deleted."
+        else
+            echo "Failed to delete hosted collector '${SUMOLOGIC_COLLECTOR_NAME}' (${COLLECTOR_ID})."
+            return 1
+        fi
     else
         echo "Hosted collector '${SUMOLOGIC_COLLECTOR_NAME}' not found, nothing to delete."
     fi
